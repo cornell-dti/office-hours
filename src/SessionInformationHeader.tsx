8ed926aea34781b6c15d1362a754d0dc946057d1
vw96 <vw96@cornell.edu>
2017-11-18 16:47:00 -0500

    Add basic header content

--- src/SessionInformationHeader.tsx
+++ src/SessionInformationHeader.tsx
@@ -5,7 +5,36 @@ class SessionInformationHeader extends React.Component {
     render() {
         return(
             <div className="SessionInformationHeader">
-                Session information placeholder
+                <div className="header">
+                    <div className="ClassInfo">
+                        <h1 id="ClassName">
+                            CS 3110 
+                        </h1>
+                        <div id="RunnerName">
+                            Michael Clarkson
+                        </div>
+                    </div>
+                    <div className="ClassLogistics">
+                        <div id="ClassTime">
+                            Wednesday, 8 Nov
+                            10:00 - 11:00 AM
+                        </div>
+                        <div id="ClassLocation">
+                            G23 Gates Hall
+                        </div>
+                    </div>
+                    <div className="QueueInfo">
+                        <div id="QueueNumber">
+                            14
+                        </div>
+                        <div id="QueueText">
+                            in queue
+                        </div>
+                    </div>
+                </div>
+                <button className="button" type="submit">
+                    Join Queue
+                </button>
             </div>
         );
     }
