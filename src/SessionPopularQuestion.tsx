a527f92a25eb81435074fce92a5f5124fd4f6272
Karun Singh <karun.singh17@gmail.com>
2017-11-19 01:03:13 -0500

    Minor changes

--- src/SessionPopularQuestion.tsx
+++ src/SessionPopularQuestion.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './SessionPopularQuestion.css';
+const peopleLogoImage = require('./peopleLogo.jpg');
 
 class SessionPopularQuestion extends React.Component {
     render() {
@@ -9,7 +10,7 @@ class SessionPopularQuestion extends React.Component {
                     How do you implement recursion on question 4?
                 </div>
                 <div className="SessionPopularQuestion-People">
-                    n: 2
+                    <img src={peopleLogoImage} className="SessionPopularQuestion-PeopleLogo" alt="3 people logo"/> 2
                 </div>
             </div>
         );
