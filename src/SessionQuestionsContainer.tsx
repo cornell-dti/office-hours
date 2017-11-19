416c093b8c6bfef8f4565f14cf3595e0e1b909a5
Karun Singh <karun.singh17@gmail.com>
2017-11-19 01:17:39 -0500

    Revert "Merge branch 'sa/sessionQuestions' of https://github.com/cornell-dti/office-hours into ks/session-popular-questions"
    This reverts commit 13ebd14cd336459c2e4bd963df224eab297f334f, reversing
    changes made to dd04c1dcc71cb47986035741fde1fe5f6f9807b6.

--- src/SessionQuestionsContainer.tsx
+++ src/SessionQuestionsContainer.tsx
@@ -1,20 +1,89 @@
 import * as React from 'react';
 import './SessionQuestionsContainer.css';
-import SessionQuestionsComponent from './SessionQuestionsComponent';
 
 class SessionQuestionsContainer extends React.Component {
     render() {
         return(
-            <div className="SessionQuestionsContainer">
-                
+            <div>
+                <text className="SessionQuestionsContainer">Session questions container placeholder</text>
                 <div>
-                    <p className="Queue">Queue</p>
+                    <text>Queue</text>
                 </div>
-                <SessionQuestionsComponent />
-                <SessionQuestionsComponent />
-                <SessionQuestionsComponent />
-                <SessionQuestionsComponent />
-                <SessionQuestionsComponent />
+                <div className="QueueQuestions">
+                    <p className="Name">Karun Singh</p>
+                    <p>How to implement recursion on question 4?</p>
+                    <div className="Tags">
+                        <p>Assignment 1</p>
+                        <p>Q4</p>
+                        <p>Recursion</p>
+                        <p>Conceptual</p>
+                    </div>
+                    <div className="BottomBar">
+                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
+                        <p className="NumberOfPeople">2</p>
+                        <button className="Button">Resolve</button>
+                    </div>
+                </div>
+                <div className="QueueQuestions">
+                    <p className="Name">Karun Singh</p>
+                    <p>How to implement recursion on question 4?</p>
+                    <div className="Tags">
+                        <p>Assignment 1</p>
+                        <p>Q4</p>
+                        <p>Recursion</p>
+                        <p>Conceptual</p>
+                    </div>
+                    <div className="BottomBar">
+                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
+                        <p className="NumberOfPeople">2</p>
+                        <button className="Button">Resolve</button>
+                    </div>
+                </div>
+                <div className="QueueQuestions">
+                    <p className="Name">Karun Singh</p>
+                    <p>How to implement recursion on question 4?</p>
+                    <div className="Tags">
+                        <p>Assignment 1</p>
+                        <p>Q4</p>
+                        <p>Recursion</p>
+                        <p>Conceptual</p>
+                    </div>
+                    <div className="BottomBar">
+                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
+                        <p className="NumberOfPeople">2</p>
+                        <button className="Button">Resolve</button>
+                    </div>
+                </div>
+                <div className="QueueQuestions">
+                    <p className="Name">Karun Singh</p>
+                    <p>How to implement recursion on question 4?</p>
+                    <div className="Tags">
+                        <p>Assignment 1</p>
+                        <p>Q4</p>
+                        <p>Recursion</p>
+                        <p>Conceptual</p>
+                    </div>
+                    <div className="BottomBar">
+                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
+                        <p className="NumberOfPeople">2</p>
+                        <button className="Button">Resolve</button>
+                    </div>
+                </div>
+                <div className="QueueQuestions">
+                    <p className="Name">Karun Singh</p>
+                    <p>How to implement recursion on question 4?</p>
+                    <div className="Tags">
+                        <p>Assignment 1</p>
+                        <p>Q4</p>
+                        <p>Recursion</p>
+                        <p>Conceptual</p>
+                    </div>
+                    <div className="BottomBar">
+                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
+                        <p className="NumberOfPeople">2</p>
+                        <button className="Button">Resolve</button>
+                    </div>
+                </div>
             </div>
         );
     }
