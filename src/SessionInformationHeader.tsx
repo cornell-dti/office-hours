530682c799dc9b8c540025394863fbff3b28d590
Karun Singh <karun.singh17@gmail.com>
2017-11-19 01:16:46 -0500

    Revert "Merge branch 'vw/SessionInformationHeader' of https://github.com/cornell-dti/office-hours into ks/session-popular-questions"
    This reverts commit 0b660505c669f643a2d4d79d33be354da0d9c062, reversing
    changes made to 13ebd14cd336459c2e4bd963df224eab297f334f.

--- src/SessionInformationHeader.tsx
+++ src/SessionInformationHeader.tsx
@@ -5,31 +5,7 @@ class SessionInformationHeader extends React.Component {
     render() {
         return(
             <div className="SessionInformationHeader">
-                <div className="header">
-                    <div className="CourseInfo">
-                        <span className="CourseNum">CS 3110  </span>
-                        Michael Clarkson
-                    </div>
-                    <div className="OfficeHourLogistics">
-                        <div className="QueueInfo"> 
-                            <div className="QueueTotal">14</div>
-                            <div>in queue</div>
-                        </div>
-                        <div className="OfficeHourInfo">
-                            <div className="OfficeHourTime">
-                                <p>Wednesday, 8 Nov</p>
-                                <p>10:00 - 11:00 AM</p>
-                            </div>
-                            <div className="OfficeHourLocation">
-                                G23 Gates Hall
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="Spacing" />
-                <button className="button" type="submit">
-                    Join Queue
-                </button>
+                Session information placeholder
             </div>
         );
     }
