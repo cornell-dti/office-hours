3b296fae5a5060efbb7c45cc13aaebf06ae09900
Karun Singh <karun.singh17@gmail.com>
2017-11-19 01:20:02 -0500

    undone merges

--- src/SessionQuestionsContainer.tsx
+++ src/SessionQuestionsContainer.tsx
@@ -4,86 +4,8 @@ import './SessionQuestionsContainer.css';
 class SessionQuestionsContainer extends React.Component {
     render() {
         return(
-            <div>
-                <text className="SessionQuestionsContainer">Session questions container placeholder</text>
-                <div>
-                    <text>Queue</text>
-                </div>
-                <div className="QueueQuestions">
-                    <p className="Name">Karun Singh</p>
-                    <p>How to implement recursion on question 4?</p>
-                    <div className="Tags">
-                        <p>Assignment 1</p>
-                        <p>Q4</p>
-                        <p>Recursion</p>
-                        <p>Conceptual</p>
-                    </div>
-                    <div className="BottomBar">
-                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
-                        <p className="NumberOfPeople">2</p>
-                        <button className="Button">Resolve</button>
-                    </div>
-                </div>
-                <div className="QueueQuestions">
-                    <p className="Name">Karun Singh</p>
-                    <p>How to implement recursion on question 4?</p>
-                    <div className="Tags">
-                        <p>Assignment 1</p>
-                        <p>Q4</p>
-                        <p>Recursion</p>
-                        <p>Conceptual</p>
-                    </div>
-                    <div className="BottomBar">
-                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
-                        <p className="NumberOfPeople">2</p>
-                        <button className="Button">Resolve</button>
-                    </div>
-                </div>
-                <div className="QueueQuestions">
-                    <p className="Name">Karun Singh</p>
-                    <p>How to implement recursion on question 4?</p>
-                    <div className="Tags">
-                        <p>Assignment 1</p>
-                        <p>Q4</p>
-                        <p>Recursion</p>
-                        <p>Conceptual</p>
-                    </div>
-                    <div className="BottomBar">
-                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
-                        <p className="NumberOfPeople">2</p>
-                        <button className="Button">Resolve</button>
-                    </div>
-                </div>
-                <div className="QueueQuestions">
-                    <p className="Name">Karun Singh</p>
-                    <p>How to implement recursion on question 4?</p>
-                    <div className="Tags">
-                        <p>Assignment 1</p>
-                        <p>Q4</p>
-                        <p>Recursion</p>
-                        <p>Conceptual</p>
-                    </div>
-                    <div className="BottomBar">
-                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
-                        <p className="NumberOfPeople">2</p>
-                        <button className="Button">Resolve</button>
-                    </div>
-                </div>
-                <div className="QueueQuestions">
-                    <p className="Name">Karun Singh</p>
-                    <p>How to implement recursion on question 4?</p>
-                    <div className="Tags">
-                        <p>Assignment 1</p>
-                        <p>Q4</p>
-                        <p>Recursion</p>
-                        <p>Conceptual</p>
-                    </div>
-                    <div className="BottomBar">
-                        {/* <img src="3-people-logo.jpg" alt="3 people logo"/> */}
-                        <p className="NumberOfPeople">2</p>
-                        <button className="Button">Resolve</button>
-                    </div>
-                </div>
+            <div className="SessionQuestionsContainer">
+                Session questions container placeholder
             </div>
         );
     }
