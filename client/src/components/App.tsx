8dc4d531bef9f16fbf5fa8f0fc271be84a2fc8e3
Karun Singh <karun.singh17@gmail.com>
2017-11-25 01:47:38 -0500

    Setting up calendar view

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import CalendarView from './pages/CalendarView';
 import StudentSessionView from './pages/StudentSessionView';
 import '../styles/App.css';
 
@@ -6,6 +7,9 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>Calendar View</h1>
+        <CalendarView />
+        <h1>Session View</h1>
         <StudentSessionView />
       </div>
     );
---
+++ client/src/components/includes/CalendarSessions.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import '../../styles/CalendarSessions.css';
+
+class CalendarSessions extends React.Component {
+    render() {
+        return (
+            <div className="CalendarSessions">
+                Calendar sessions placeholder
+            </div>
+        );
+    }
+}
+
+export default CalendarSessions;
