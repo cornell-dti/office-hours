fdf62082a869975590c346112f5ab2a8dc2e957f
vw96 <vw96@cornell.edu>
2017-11-25 17:08:23 -0500

    Add and arrange components

--- client/src/components/includes/CalendarWeekSelect.tsx
+++ client/src/components/includes/CalendarWeekSelect.tsx
@@ -5,7 +5,21 @@ class CalendarWeekSelect extends React.Component {
     render() {
         return (
             <div className="CalendarWeekSelect">
-                Calendar week select placeholder
+                <div className="CurrentWeek">
+                    <div className="ThisWeek">
+                        This Week
+                    </div>
+                    <div>
+                        10 - 16 November
+                    </div>
+                </div>
+                <div className="NextWeek">
+                    16 - 22 November
+                    <button className="NextButton">
+                        >
+                </button>
+                </div>
+
             </div>
         );
     }
