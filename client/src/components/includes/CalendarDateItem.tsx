3bd1f7fe32fab763ec0626d3778c3b040d4f2810
Karun Singh <karun.singh17@gmail.com>
2017-11-25 23:43:26 -0500

    Initial front-end work

---
+++ client/src/components/includes/CalendarDateItem.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+class CalendarDateItem extends React.Component {
+    props: {
+        active: boolean,
+        day: string,
+        date: number,
+        hasOH: boolean
+    };
+    render() {
+        var activeClass = 'menuDate';
+        if (this.props.active === true) {
+            activeClass = 'menuDate active';
+        }
+        var notifText = '\u00B7';
+        if (this.props.hasOH === false) {
+            notifText = '';
+        }
+        return (
+            <div className={activeClass}>
+                <div className="notification">{notifText}</div>
+                <div className="day">{this.props.day}</div>
+                <div className="date">{this.props.date}</div>
+            </div>
+        );
+    }
+}
+
+export default CalendarDateItem;
--- client/src/components/includes/CalendarDateSelect.tsx
+++ client/src/components/includes/CalendarDateSelect.tsx
@@ -1,11 +1,22 @@
 import * as React from 'react';
 import '../../styles/CalendarDateSelect.css';
+import CalendarDateItem from './CalendarDateItem';
 
 class CalendarDateSelect extends React.Component {
+
     render() {
         return (
             <div className="CalendarDateSelect">
-                Calendar date select placeholder
+                <div className="CalendarDateSelect-Month">Nov, 2017</div>
+                <div className="CalendarDateSelect-Dates">
+                    <CalendarDateItem day="Mon" date={10} hasOH={true} active={true} />
+                    <CalendarDateItem day="Tue" date={11} hasOH={false} active={false} />
+                    <CalendarDateItem day="Wed" date={12} hasOH={true} active={false} />
+                    <CalendarDateItem day="Thu" date={13} hasOH={true} active={false} />
+                    <CalendarDateItem day="Fri" date={14} hasOH={false} active={false} />
+                    <CalendarDateItem day="Sat" date={15} hasOH={false} active={false} />
+                    <CalendarDateItem day="Sun" date={16} hasOH={false} active={false} />
+                </div>
             </div>
         );
     }
