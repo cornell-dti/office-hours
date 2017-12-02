5882fa460ab4e41d2fbe7c32e61de4529a44952b
Shefali Agarwal <sa652@cornell.edu>
2017-12-02 17:36:56 -0500

    final changes to add question

--- client/src/components/includes/QuestionHeader.tsx
+++ client/src/components/includes/QuestionHeader.tsx
@@ -8,36 +8,40 @@ class QuestionHeader extends React.Component {
           <div className="QuestionHeader">
             Add Your Question
             <div className="header">
-              <div className="CourseInfo">
-                  <span className="CourseNum">CS 3110</span>
+              <div className="QuestionCourseInfo">
+                  <span className="QuestionCourseNum">CS 3110</span>
                   Michael Clarkson
               </div>
             </div>
             <div className="tagsContainer">
+              <p className="x">x</p>
               <div className="tagsMiniContainer">
                 <p>Primary Tags</p>
-                <div className="Tags">
-                    <p>Assignment 1</p>
+                <div className="QuestionTags">
+                    <p className="selectedTag">Assignment 1</p>
                     <p>Assignment 2</p>
                     <p>Prelim 1 Feedback</p>
                 </div>
+              </div>
+              <div className="tagsMiniContainer">
                 <p>Secondary Tags</p>
-                <div className="Tags">
-                    <p>Q1</p>
+                <div className="QuestionTags">
+                    <p className="selectedTag">Q1</p>
                     <p>Q2</p>
                     <p>Q3</p>
                     <p>Q4</p>
                     <p>Q5</p>
                     <p>Conceptual</p>
-                    <p>Clarification</p>
+                    <p className="selectedTag">Clarification</p>
                     <p>Recursion</p>
                     <p>Conditional</p>
                     <p>Data</p>
                     <p>Debugging</p>
                 </div>
               </div>
-              <div className="tagsMiniContainer">
+              <div className="tagsMiniContainer2">
                 <p>Question</p>
+                <p className="QuestionInput">What do you want to ask about?</p>
               </div>
             </div>
           </div>
