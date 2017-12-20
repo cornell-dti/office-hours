80c6e67860bde0f0d75eb5cd0727656b8ed11e6c
Karun Singh <karun.singh17@gmail.com>
2017-12-20 10:19:53 -0500

    Initial packages setup

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,17 +1,29 @@
 import * as React from 'react';
 import CalendarView from './pages/CalendarView';
 import StudentSessionView from './pages/StudentSessionView';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import gql from 'graphql-tag';
 import '../styles/App.css';
+
+const client = new ApolloClient({
+  link: new HttpLink({ uri: 'http://localhost:3001/graphql' }),
+  cache: new InMemoryCache()
+});
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <h1>Calendar View</h1>
-        <CalendarView />
-        <h1>Session View</h1>
-        <StudentSessionView />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Calendar View</h1>
+          <CalendarView />
+          <h1>Session View</h1>
+          <StudentSessionView />
+        </div>
+      </ApolloProvider>
     );
   }
 }
