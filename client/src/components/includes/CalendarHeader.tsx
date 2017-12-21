a1d291184dcc0cd7b0783bbfd35eec3191335183
Karun Singh <karun.singh17@gmail.com>
2017-12-21 09:59:40 -0500

    Integrating GraphQL into CalendarSessions

--- client/src/components/includes/CalendarHeader.tsx
+++ client/src/components/includes/CalendarHeader.tsx
@@ -1,9 +1,7 @@
 import * as React from 'react';
 import '../../styles/CalendarHeader.css';
-import gql from 'graphql-tag';
-import { graphql, ChildProps } from 'react-apollo';
 
-/*class CalendarHeader extends React.Component {
+class CalendarHeader extends React.Component {
     props: {
         currentCourse: string
     };
@@ -23,55 +21,6 @@ import { graphql, ChildProps } from 'react-apollo';
             </div>
         );
     }
-}*/
-
-const COURSE_QUERY = gql`
-query getCourseDetails($c:String!) {
-    course(name: $c) {
-        name
-        semester
-    }
-}`;
-
-type Response = {
-    course: {
-        name: string,
-        semester: string
-    }
-};
-
-type InputProps = {
-    currentCourse: string
-};
-
-const finalQuery = graphql<Response, InputProps>(COURSE_QUERY, {
-    options: ({ currentCourse }) => ({
-        variables: { c: currentCourse }
-    }),
-});
-
-class CalendarHeader extends React.Component<ChildProps<InputProps, Response>, {}> {
-    render() {
-        if (this.props.data.loading) {
-            return <div>Loading</div>;
-        }
-        if (this.props.data.error) {
-            return <h1>ERROR</h1>;
-        }
-        return (
-            <div className="CalendarHeader">
-                <div className="CurrentCourse">
-                    {this.props.data.course.name + ', ' + this.props.data.course.semester}
-                    <button className="CourseSelectButton">
-                        <i className="angle down icon" />
-                    </button>
-                </div>
-                <button className="MenuButton">
-                    <i className="bars icon" />
-                </button>
-            </div>
-        );
-    }
 }
 
-export default finalQuery(CalendarHeader);
+export default CalendarHeader;
--- client/src/components/pages/CalendarView.tsx
+++ client/src/components/pages/CalendarView.tsx
@@ -15,7 +15,7 @@ class CalendarView extends React.Component {
             <div className="CalendarView">
                 <CalendarHeader currentCourse="CS2800" />
                 <CalendarDateSelect dayList={days} dateList={dates} hasOHList={hasOHs} />
-                <CalendarSessions />
+                <CalendarSessions currentCourse="CS2800" />
                 <CalendarWeekSelect
                     thisWeek="10 - 16 November"
                     nextWeek="16 - 22 November"
