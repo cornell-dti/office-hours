a76356cd2d21198905d0c5648fc6848cc3108674
Karun Singh <karun.singh17@gmail.com>
2017-12-21 04:56:29 -0500

    Rendering actual dates/weeks

--- client/src/components/pages/CalendarView.tsx
+++ client/src/components/pages/CalendarView.tsx
@@ -5,20 +5,70 @@ import CalendarDateSelect from '../includes/CalendarDateSelect';
 import CalendarSessions from '../includes/CalendarSessions';
 import CalendarWeekSelect from '../includes/CalendarWeekSelect';
 
-class CalendarView extends React.Component {
+class CalendarView extends React.Component<{}, {}> {
+
+    monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'];
+
+    state: {
+        currentEpoch: number
+    };
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { currentEpoch: new Date().getTime() };
+    }
+
+    componentWillMount() {
+        // determine initial state here
+    }
+
+    getWeekText(epoch: number): string {
+        var now = new Date(epoch);
+        var weekText = '';
+        weekText += now.getDate();
+        weekText += ' - ';
+        now.setTime(now.getTime() +
+            6 /* days */ * 24 /* hours */ * 60 /* minutes */ * 60 /* seconds */ * 1000 /* millis */);
+        weekText += now.getDate();
+        weekText += ' ';
+        weekText += this.monthNames[now.getMonth()];
+        return weekText;
+    }
+
     render() {
-        const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-        const dates = [10, 11, 12, 13, 14, 15, 16];
-        const hasOHs = [true, false, true, true, false, false, false];
+        var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        var hasOHs = [false, false, false, false, false, false, false];
+        var dates = [];
+
+        var now = new Date(this.state.currentEpoch);
+        var monthYear = this.monthNames[now.getMonth()] + ' ' + now.getFullYear();
+
+        var todayIndex = now.getDay();
+        days = days.slice(todayIndex).concat(days.splice(0, todayIndex));
+
+        for (var i = 0; i < 7; i++) {
+            dates.push(now.getDate());
+            now.setTime(now.getTime() + 24 /* hours */ * 60 /* minutes */ * 60 /* seconds */ * 1000 /* millis */);
+        }
+
+        const thisWeekText = this.getWeekText(this.state.currentEpoch);
+        const nextWeekText = this.getWeekText(this.state.currentEpoch +
+            7 /* days */ * 24 /* hours */ * 60 /* minutes */ * 60 /* seconds */ * 1000 /* millis */);
 
         return (
             <div className="CalendarView">
                 <CalendarHeader currentCourse="CS 3110" />
-                <CalendarDateSelect dayList={days} dateList={dates} hasOHList={hasOHs} />
+                <CalendarDateSelect
+                    dayList={days}
+                    dateList={dates}
+                    hasOHList={hasOHs}
+                    monthYear={monthYear}
+                />
                 <CalendarSessions />
                 <CalendarWeekSelect
-                    thisWeek="10 - 16 November"
-                    nextWeek="16 - 22 November"
+                    thisWeek={thisWeekText}
+                    nextWeek={nextWeekText}
                 />
             </div>
         );
