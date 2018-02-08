42f4b95f2bc98ebfd76023e288e1d101aa0edac5
Karun Singh <karun.singh17@gmail.com>
2018-02-07 19:19:04 -0500

    ts -- bug

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,12 @@
 import * as express from 'express';
-import postgraphql from 'postgraphql';
+//import postgraphql from 'postgraphql';
 
+console.log("hi");
 const app = express();
 
-app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', { graphiql: true }));
-app.use(express.static('build'))
+//app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', { graphiql: true }));
+app.use(express.static('../client/build'))
 
-app.listen(process.env.PORT || 3001);
+app.listen(process.env.PORT || 3001, () => {
+    console.log("served bro");
+});
