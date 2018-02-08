b36d12936a3e4b16ae5d62d4fa4488b0f4ac2513
Karun Singh <karun.singh17@gmail.com>
2018-02-07 19:36:34 -0500

    path

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,11 @@
 import * as express from 'express';
+import path from 'path';
 //import postgraphql from 'postgraphql';
 
-console.log("hi");
 const app = express();
 
 //app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', { graphiql: true }));
-app.use(express.static('../client/build'))
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 app.listen(process.env.PORT || 3001, () => {
     console.log("served bro");
