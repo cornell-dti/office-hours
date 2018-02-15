e01458c29101bad4cd076d56c1532b00b0392017
Ryan Slama <rockinryguy@gmail.com>
2018-02-14 20:33:17 -0500

    Button styling

--- client/src/components/pages/LoginView.tsx
+++ client/src/components/pages/LoginView.tsx
@@ -17,6 +17,18 @@ const hintText: any = {
   color: 'grey'
 }
 
+const btnStyle: any = {
+  backgroundImage: 'linear-gradient(-90deg, #83C5EA 0%, #5D89F0 100%)',
+  boxShadow: '0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.20)',
+  borderRadius: '4px',
+  padding: '12px',
+  width: '200px',
+  fontSize: '16px',
+  color: 'white',
+  outline: 'none',
+  transition: 'all 0.17s cubic-bezier(0.4, 0.0, 0.2, 1)',
+}
+
 class LoginView extends React.Component {
   printResponse = (response: ReactGoogleLogin.GoogleLoginResponseOffline) => {
     console.log(response.code);
@@ -35,11 +47,12 @@ class LoginView extends React.Component {
         <p style={hintText} >Use your Cornell NetID to login</p>
         <GoogleLogin
           clientId="694487664328-79nbgbrnm3n3sa3nfsdfm5jigkr69svp.apps.googleusercontent.com"
-          buttonText="Login"
+          buttonText="Sign in with Google"
           onSuccess={this.printResponse}
           onFailure={this.printError}
           hostedDomain="cornell.edu"
           responseType="code"
+          style={btnStyle}
         />
       </div>
     );
