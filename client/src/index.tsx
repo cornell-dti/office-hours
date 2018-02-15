83762c4d2fb477d3e208ff04ae3ed217a77d869c
Ryan Slama <rockinryguy@gmail.com>
2018-02-14 19:02:25 -0500

    Add Apollo

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,11 +2,15 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 import registerServiceWorker from './components/includes/registerServiceWorker';
+import { ApolloProvider } from 'react-apollo';
 import './styles/index.css';
 import 'semantic-ui-css/semantic.min.css';
+import { client } from './components/includes/ApolloClient'
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
