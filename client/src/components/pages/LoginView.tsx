3cf714edcd205ba938b42fb64fd3c54910200585
Ryan Slama <rockinryguy@gmail.com>
2018-02-22 11:15:51 -0500

    Added SVGs to Login Form and tweaked gradients to comply with Sketch file

--- client/src/components/pages/LoginView.tsx
+++ client/src/components/pages/LoginView.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import GoogleLogin from 'react-google-login';
+const googleLogo = require('../../media/googleLogo.svg');
+const QMeLogo = require('../../media/QMeLogo.svg');
+const QLogo = require('../../media/QLogo.svg');
 
 const topStyle: {} = {
   color: 'white',
   height: '45vh',
   fontSize: '18px',
   lineHeight: '1.4',
-  backgroundImage: 'linear-gradient(-225deg, #6DB9EA 0%, #668AE9 100%)',
+  backgroundImage: 'linear-gradient(-46deg, #668AE9 0%, #6DB9EA 100%)',
   marginBottom: '40px',
   display: 'flex',
   justifyContent: 'center',
-  alignItems: 'center'
+  alignItems: 'center',
+  flexDirection: 'column',
 };
 
 const hintText: {} = {
@@ -18,15 +22,40 @@ const hintText: {} = {
 };
 
 const btnStyle: {} = {
-  backgroundImage: 'linear-gradient(90deg, #83C5EA 0%, #5D89F0 100%)',
-  boxShadow: '0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.20)',
-  borderRadius: '4px',
-  padding: '12px',
-  width: '200px',
+  backgroundImage: 'linear-gradient(-135deg, #6DB9EA 0%, #668AE9 100%)',
+  boxShadow: '0 0 2px 0 rgba(0,0,0,0.12), 0 2px 2px 0 rgba(0,0,0,0.24)',
+  borderRadius: '2px',
+  padding: '1px',
+  width: '247px',
   fontSize: '16px',
-  color: 'white',
   outline: 'none',
   transition: 'all 0.17s cubic-bezier(0.4, 0.0, 0.2, 1)',
+};
+
+const QMeLogoStyle: {} = {
+  display: 'block',
+  paddingBottom: '40px'
+};
+
+const googleLogoStyle: {} = {
+  float: 'left'
+};
+
+const textStyle: {} = {
+  color: 'white',
+  fontFamily: 'Roboto',
+  width: '100%',
+  display: 'block',
+  paddingTop: '10px'
+};
+
+const QLogoStyle: {} = {
+  bottom: '20px',
+  position: 'absolute',
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  left: 0,
+  right: 0
 };
 
 class LoginView extends React.Component {
@@ -42,19 +71,24 @@ class LoginView extends React.Component {
     return (
       <div className="LoginView">
         <section style={topStyle}>
+          <img src={QMeLogo} style={QMeLogoStyle} />
           Queue up for office hours remotely. <br /> Skip the wait line
         </section>
         <p style={hintText} >Use your Cornell NetID to login</p>
         <GoogleLogin
           hostedDomain="cornell.edu"
           clientId="694487664328-79nbgbrnm3n3sa3nfsdfm5jigkr69svp.apps.googleusercontent.com"
-          buttonText="Sign in with Google"
           onSuccess={this.printResponse}
           onFailure={this.printError}
           responseType="code"
           style={btnStyle}
-        />
-      </div>
+          prompt=""
+        >
+          <img src={googleLogo} style={googleLogoStyle} />
+          <span style={textStyle}>Sign in with Google</span>
+        </GoogleLogin>
+        <img src={QLogo} style={QLogoStyle} />
+      </div >
     );
   }
 }
--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,17 @@
 import * as express from 'express';
 import postgraphql from 'postgraphql';
+import bodyParser from 'body-parser';
+import { graphqlExpress } from 'apollo-server-express';
 
 const app = express();
 
 // app.use(postgraphql('postgres://localhost:5432', { graphiql: true }));
+
+var schema = `
+    type Query {
+        token: String
+    }
+`;
+app.use('/auth', bodyParser.json(), graphqlExpress({ schema: schema }));
 
 app.listen(3001);
