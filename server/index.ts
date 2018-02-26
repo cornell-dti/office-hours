dda3455916b4dac30f64b2be80f783db26cd8368
Karun Singh <karun.singh17@gmail.com>
2018-02-25 22:13:48 -0500

    Setting up build pipeline

--- server/index.ts
+++ server/index.ts
@@ -5,8 +5,8 @@ import postgraphql from 'postgraphql';
 const app = express();
 
 app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', { graphiql: true }));
-// app.use(express.static('../client/build'))
+app.use(express.static('../client/build'))
 
-app.listen(process.env.PORT || 3001, () => {
-    console.log("Now listening on port " + process.env.PORT || 3001);
+app.listen((process.env.PORT || 3001), () => {
+    console.log("Now listening on port " + (process.env.PORT || 3001));
 });
