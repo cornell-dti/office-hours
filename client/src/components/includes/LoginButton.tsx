d50fcc1e3fa79e5d85c797f1e9b261945e955167
Ryan Slama <rockinryguy@gmail.com>
2018-02-28 19:06:38 -0500

    Converted styling to Less

--- client/src/components/includes/LoginButton.tsx
+++ client/src/components/includes/LoginButton.tsx
@@ -1,32 +1,6 @@
 import * as React from 'react';
 const googleLogo = require('../../media/googleLogo.svg');
 
-const googleLogoStyle: {} = {
-    float: 'left'
-};
-
-const textStyle: {} = {
-    color: 'white',
-    fontFamily: 'Roboto',
-    width: '100%',
-    display: 'block',
-    paddingTop: '10px'
-};
-
-const btnStyle: {} = {
-    backgroundImage: 'linear-gradient(-135deg, #6DB9EA 0%, #668AE9 100%)',
-    boxShadow: '0 0 2px 0 rgba(0,0,0,0.12), 0 2px 2px 0 rgba(0,0,0,0.24)',
-    borderRadius: '2px',
-    padding: '1px',
-    width: '247px',
-    fontSize: '16px',
-    outline: 'none',
-    transition: 'all 0.17s cubic-bezier(0.4, 0.0, 0.2, 1)',
-    display: 'block',
-    margin: 'auto',
-    height: '40px'
-};
-
 class LoginButton extends React.Component {
 
     props: {
@@ -35,12 +9,9 @@ class LoginButton extends React.Component {
 
     render() {
         return (
-            <a
-                style={btnStyle}
-                href={this.props.URL}
-            >
-                <img src={googleLogo} style={googleLogoStyle} />
-                <span style={textStyle}>Sign in with Google</span>
+            <a className="loginButton" href={this.props.URL} >
+                <img src={googleLogo} className="googleLogo" />
+                <span className="loginButtonText">Sign in with Google</span>
             </a>
         );
     }
--- client/src/components/pages/LoginView.tsx
+++ client/src/components/pages/LoginView.tsx
@@ -3,48 +3,17 @@ const QMeLogo = require('../../media/QMeLogo.svg');
 const QLogo = require('../../media/QLogo.svg');
 import LoginButton from '../includes/LoginButton';
 
-const topStyle: {} = {
-  color: 'white',
-  height: '45vh',
-  fontSize: '18px',
-  lineHeight: '1.4',
-  backgroundImage: 'linear-gradient(-46deg, #668AE9 0%, #6DB9EA 100%)',
-  marginBottom: '40px',
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  flexDirection: 'column',
-};
-
-const hintText: {} = {
-  color: 'grey'
-};
-
-const QMeLogoStyle: {} = {
-  display: 'block',
-  paddingBottom: '40px'
-};
-
-const QLogoStyle: {} = {
-  bottom: '20px',
-  position: 'absolute',
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  left: 0,
-  right: 0
-};
-
 class LoginView extends React.Component {
   render() {
     return (
       <div className="LoginView">
-        <section style={topStyle}>
-          <img src={QMeLogo} style={QMeLogoStyle} />
+        <section className="topPanel">
+          <img src={QMeLogo} className="QMeLogo" />
           Queue up for office hours remotely. <br /> Skip the wait line
         </section>
-        <p style={hintText} >Use your Cornell NetID to login</p>
+        <p className="hintText" >Use your Cornell NetID to login</p>
         <LoginButton URL="http://localhost:3001/auth" />
-        <img src={QLogo} style={QLogoStyle} />
+        <img src={QLogo} className="QLogo" />
       </div >
     );
   }
