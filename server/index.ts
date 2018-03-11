5b2412808c3335dc00af3facdaaf6fdd81a6e00b
Horace He <horacehe2007@yahoo.com>
2018-03-11 14:09:43 -0400

    started adding mocking data

--- server/index.ts
+++ server/index.ts
@@ -5,6 +5,10 @@ import * as path from 'path';
 import postgraphql from 'postgraphql';
 import * as passport from 'passport';
 
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import { graphql } from 'graphql';
+import { readFileSync, readFile } from 'fs'
+
 const app = express();
 
 // Initialize session middleware
@@ -27,71 +31,23 @@ app.use(passport.initialize());
 app.use(passport.session());
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', {
-    graphiql: true,
-    graphqlRoute: '/__gql/graphql',
-    graphiqlRoute: '/__gql/graphiql'
-}));
+// app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', {
+//     graphiql: true,
+//     graphqlRoute: '/__gql/graphql',
+//     graphiqlRoute: '/__gql/graphiql'
+// }));
 
-passport.use(new GoogleStrategy(
-    {
-        clientID: "694487664328-79nbgbrnm3n3sa3nfsdfm5jigkr69svp.apps.googleusercontent.com",
-        clientSecret: process.env.OH_GOOGLE_SECRET,
-        callbackURL: "/__auth/callback",
-        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
-    },
-    function (accessToken, refreshToken, profile, cb) {
-        console.log(profile.id)
-        return (cb(null, profile))
-        // User.findOrCreate({ googleId: profile.id }, function (err, user) {
-        //     return cb(err, user);
-        // });
-    }
-))
+// Fill this in with the schema string
+const schemaString = readFileSync('schema.gql').toString();
 
-passport.serializeUser(function (user, done) {
-    // @ts-ignore
-    done(null, user.id)
-})
+// Make a GraphQL schema with no resolvers
+const schema = makeExecutableSchema({ typeDefs: schemaString });
 
-passport.deserializeUser(function (user, done) {
-    done(null, user)
-})
+// Add mocks, modifies schema in place
+addMockFunctionsToSchema({ schema });
 
-app.get('/__auth',
-    passport.authenticate('google', {
-        scope: ['email'],
-        // @ts-ignore: Hosted domain is used by the Google strategy, but not allowed in passport's types
-        hostedDomain: "cornell.edu"
-    })
-)
 
-app.get('/__auth/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    function (req, res) {
-        console.log("Callback:")
-        console.log(req.user)
-        res.redirect('/');
-    }
-)
-
-app.get('/__sess',
-    function (req, res) {
-        console.log("####################")
-        console.log("Session Test:")
-        console.log(req.user)
-        if (req.user == undefined) {
-            res.redirect('/login');
-        }
-        res.send(req.user)
-    }
-)
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
-app.get('*', function (req, res) {
-    res.sendFile(path.resolve(__dirname, '../client/build/index.html'));
-});
-
+app.use(express.static('../client/build'));
 app.listen(process.env.PORT || 3001, () => {
     console.log("Now listening on port " + (process.env.PORT || 3001));
 });
