2345f3429d5a80db757807e5bac5609c69d97cc8
Horace He <horacehe2007@yahoo.com>
2018-03-11 14:18:30 -0400

    Added super mock data

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
 import * as express from 'express';
 import * as session from 'express-session';
-
+import * as bodyparser from 'body-parser';
 import * as path from 'path';
 import postgraphql from 'postgraphql';
 import * as passport from 'passport';
 
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express'
 import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
 import { graphql } from 'graphql';
 import { readFileSync, readFile } from 'fs'
@@ -31,13 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 var GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-// app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', {
-//     graphiql: true,
-//     graphqlRoute: '/__gql/graphql',
-//     graphiqlRoute: '/__gql/graphiql'
-// }));
 
-// Fill this in with the schema string
 const schemaString = readFileSync('schema.gql').toString();
 
 // Make a GraphQL schema with no resolvers
@@ -45,7 +40,25 @@ const schema = makeExecutableSchema({ typeDefs: schemaString });
 
 // Add mocks, modifies schema in place
 addMockFunctionsToSchema({ schema });
-
+app.use(
+    '/__gql/graphql',
+    bodyparser.json(),
+    graphqlExpress({
+        schema: schema,
+        context: {}, // at least(!) an empty object
+    })
+);
+app.use(
+    '/__gql/graphiql',
+    graphiqlExpress({
+        endpointURL: '/__gql/graphql',
+    })
+);
+// app.use(postgraphql(process.env.DATABASE_URL || 'postgres://localhost:5432', {
+//     graphiql: true,
+//     graphqlRoute: '/__gql/graphql',
+//     graphiqlRoute: '/__gql/graphiql'
+// }));
 
 app.use(express.static('../client/build'));
 app.listen(process.env.PORT || 3001, () => {
