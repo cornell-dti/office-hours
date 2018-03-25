b0cc92d780b3c4a1608cdfa2e91cf968a8eb9882
Horace He <horacehe2007@yahoo.com>
2018-03-24 23:12:53 -0400

    Added new office hours schema

--- server/office_hours.sql
+++ server/office_hours.sql
@@ -1,521 +1,161 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 10.2
--- Dumped by pg_dump version 10.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-SET row_security = off;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: course_users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE course_users (
-    course_id integer NOT NULL,
-    user_id text NOT NULL,
-    status text
+CREATE TABLE "users" (
+	"userId" serial NOT NULL UNIQUE,
+	"netId" TEXT NOT NULL UNIQUE,
+	"googleId" TEXT NOT NULL UNIQUE,
+	"firstName" TEXT,
+	"lastName" TEXT,
+	"createdAt" TIMESTAMP,
+	"lastActivityAt" TIMESTAMP,
+	CONSTRAINT users_pk PRIMARY KEY ("userId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: courses; Type: TABLE; Schema: public; Owner: -
---
 
-CREATE TABLE courses (
-    course_id integer NOT NULL,
-    code text,
-    name text,
-    semester text
+CREATE TABLE "courses" (
+	"courseId" serial NOT NULL,
+	"code" TEXT NOT NULL,
+	"name" TEXT NOT NULL,
+	"semester" TEXT NOT NULL,
+	"startDate" DATE NOT NULL,
+	"endDate" DATE NOT NULL,
+	CONSTRAINT courses_pk PRIMARY KEY ("courseId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: courses_course_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
-CREATE SEQUENCE courses_course_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: courses_course_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE courses_course_id_seq OWNED BY courses.course_id;
-
-
---
--- Name: question_followers; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE question_followers (
-    question_id integer NOT NULL,
-    follower text NOT NULL
+CREATE TABLE "questions" (
+	"questionId" serial NOT NULL,
+	"content" TEXT NOT NULL,
+	"timeEntered" TIMESTAMP NOT NULL,
+	"status" TEXT NOT NULL,
+	"timeResolved" TIMESTAMP,
+	"sessionId" integer NOT NULL,
+	"askerId" integer NOT NULL,
+	"answererId" integer,
+	CONSTRAINT questions_pk PRIMARY KEY ("questionId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: question_tags; Type: TABLE; Schema: public; Owner: -
---
 
-CREATE TABLE question_tags (
-    tag_id integer NOT NULL,
-    question_id integer NOT NULL
+CREATE TABLE "sessionSeries" (
+	"sessionSeriesId" serial NOT NULL,
+	"startTime" TIMESTAMP NOT NULL,
+	"endTime" TIMESTAMP NOT NULL,
+	"location" TEXT NOT NULL,
+	"courseId" integer NOT NULL,
+	CONSTRAINT sessionSeries_pk PRIMARY KEY ("sessionSeriesId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: questions; Type: TABLE; Schema: public; Owner: -
---
 
-CREATE TABLE questions (
-    question_id integer NOT NULL,
-    value text,
-    time_entered timestamp without time zone DEFAULT now(),
-    status text,
-    time_resolved timestamp without time zone,
-    session_id integer,
-    student text
+CREATE TABLE "sessions" (
+	"sessionId" serial NOT NULL,
+	"startTime" TIMESTAMP,
+	"endTime" TIMESTAMP,
+	"location" TEXT,
+	"sessionSeriesId" integer,
+	CONSTRAINT sessions_pk PRIMARY KEY ("sessionId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: questions_question_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
-CREATE SEQUENCE questions_question_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: questions_question_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE questions_question_id_seq OWNED BY questions.question_id;
-
-
---
--- Name: session_tas; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE session_tas (
-    session_id integer NOT NULL,
-    ta text NOT NULL
+CREATE TABLE "tags" (
+	"tagId" serial NOT NULL,
+	"name" TEXT NOT NULL,
+	"courseId" integer NOT NULL,
+	"level" integer NOT NULL,
+	CONSTRAINT tags_pk PRIMARY KEY ("tagId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: sessions; Type: TABLE; Schema: public; Owner: -
---
 
-CREATE TABLE sessions (
-    session_id integer NOT NULL,
-    start_time timestamp without time zone,
-    end_time timestamp without time zone,
-    location text,
-    course_id integer
+CREATE TABLE "tagRelations" (
+	"parentId" integer NOT NULL,
+	"childId" integer NOT NULL,
+	CONSTRAINT tagRelations_pk PRIMARY KEY ("parentId","childId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: sessions_session_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
-CREATE SEQUENCE sessions_session_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: sessions_session_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE sessions_session_id_seq OWNED BY sessions.session_id;
-
-
---
--- Name: tags; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE tags (
-    tag_id integer NOT NULL,
-    value text,
-    course_id integer
+CREATE TABLE "courseUsers" (
+	"courseId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	"role" TEXT NOT NULL,
+	CONSTRAINT courseUsers_pk PRIMARY KEY ("courseId","userId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: tags_tag_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
 
-CREATE SEQUENCE tags_tag_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: tags_tag_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE tags_tag_id_seq OWNED BY tags.tag_id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE users (
-    netid text NOT NULL,
-    name text
+CREATE TABLE "questionTags" (
+	"questionId" integer NOT NULL,
+	"tagId" integer NOT NULL,
+	CONSTRAINT questionTags_pk PRIMARY KEY ("questionId","tagId")
+) WITH (
+  OIDS=FALSE
 );
 
 
---
--- Name: courses course_id; Type: DEFAULT; Schema: public; Owner: -
---
 
-ALTER TABLE ONLY courses ALTER COLUMN course_id SET DEFAULT nextval('courses_course_id_seq'::regclass);
+CREATE TABLE "sessionTas" (
+	"sessionId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	CONSTRAINT sessionTas_pk PRIMARY KEY ("sessionId","userId")
+) WITH (
+  OIDS=FALSE
+);
 
 
---
--- Name: questions question_id; Type: DEFAULT; Schema: public; Owner: -
---
 
-ALTER TABLE ONLY questions ALTER COLUMN question_id SET DEFAULT nextval('questions_question_id_seq'::regclass);
+CREATE TABLE "sessionSeriesTas" (
+	"sessionSeriesId" integer NOT NULL,
+	"userId" integer NOT NULL,
+	CONSTRAINT sessionSeriesTas_pk PRIMARY KEY ("sessionSeriesId","userId")
+) WITH (
+  OIDS=FALSE
+);
 
 
---
--- Name: sessions session_id; Type: DEFAULT; Schema: public; Owner: -
---
 
-ALTER TABLE ONLY sessions ALTER COLUMN session_id SET DEFAULT nextval('sessions_session_id_seq'::regclass);
 
 
---
--- Name: tags tag_id; Type: DEFAULT; Schema: public; Owner: -
---
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk0" FOREIGN KEY ("sessionId") REFERENCES "sessions"("sessionId");
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk1" FOREIGN KEY ("askerId") REFERENCES "users"("userId");
+ALTER TABLE "questions" ADD CONSTRAINT "questions_fk2" FOREIGN KEY ("answererId") REFERENCES "users"("userId");
 
-ALTER TABLE ONLY tags ALTER COLUMN tag_id SET DEFAULT nextval('tags_tag_id_seq'::regclass);
+ALTER TABLE "sessionSeries" ADD CONSTRAINT "sessionSeries_fk0" FOREIGN KEY ("courseId") REFERENCES "courses"("courseId");
 
+ALTER TABLE "sessions" ADD CONSTRAINT "sessions_fk0" FOREIGN KEY ("sessionSeriesId") REFERENCES "sessionSeries"("sessionSeriesId");
 
---
--- Data for Name: course_users; Type: TABLE DATA; Schema: public; Owner: -
---
+ALTER TABLE "tags" ADD CONSTRAINT "tags_fk0" FOREIGN KEY ("courseId") REFERENCES "courses"("courseId");
 
-COPY course_users (course_id, user_id, status) FROM stdin;
-\.
+ALTER TABLE "tagRelations" ADD CONSTRAINT "tagRelations_fk0" FOREIGN KEY ("parentId") REFERENCES "tags"("tagId");
+ALTER TABLE "tagRelations" ADD CONSTRAINT "tagRelations_fk1" FOREIGN KEY ("childId") REFERENCES "tags"("tagId");
 
+ALTER TABLE "courseUsers" ADD CONSTRAINT "courseUsers_fk0" FOREIGN KEY ("courseId") REFERENCES "courses"("courseId");
+ALTER TABLE "courseUsers" ADD CONSTRAINT "courseUsers_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 
---
--- Data for Name: courses; Type: TABLE DATA; Schema: public; Owner: -
---
+ALTER TABLE "questionTags" ADD CONSTRAINT "questionTags_fk0" FOREIGN KEY ("questionId") REFERENCES "questions"("questionId");
+ALTER TABLE "questionTags" ADD CONSTRAINT "questionTags_fk1" FOREIGN KEY ("tagId") REFERENCES "tags"("tagId");
 
-COPY courses (course_id, code, name, semester) FROM stdin;
-\.
+ALTER TABLE "sessionTas" ADD CONSTRAINT "sessionTas_fk0" FOREIGN KEY ("sessionId") REFERENCES "sessions"("sessionId");
+ALTER TABLE "sessionTas" ADD CONSTRAINT "sessionTas_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 
-
---
--- Data for Name: question_followers; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY question_followers (question_id, follower) FROM stdin;
-\.
-
-
---
--- Data for Name: question_tags; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY question_tags (tag_id, question_id) FROM stdin;
-\.
-
-
---
--- Data for Name: questions; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY questions (question_id, value, time_entered, status, time_resolved, session_id, student) FROM stdin;
-1	hey	\N	\N	\N	\N	\N
-2	hey	\N	\N	\N	\N	\N
-3	hey	\N	\N	\N	\N	\N
-\.
-
-
---
--- Data for Name: session_tas; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY session_tas (session_id, ta) FROM stdin;
-\.
-
-
---
--- Data for Name: sessions; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY sessions (session_id, start_time, end_time, location, course_id) FROM stdin;
-\.
-
-
---
--- Data for Name: tags; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY tags (tag_id, value, course_id) FROM stdin;
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: -
---
-
-COPY users (netid, name) FROM stdin;
-\.
-
-
---
--- Name: courses_course_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('courses_course_id_seq', 1, false);
-
-
---
--- Name: questions_question_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('questions_question_id_seq', 3, true);
-
-
---
--- Name: sessions_session_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('sessions_session_id_seq', 1, false);
-
-
---
--- Name: tags_tag_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
---
-
-SELECT pg_catalog.setval('tags_tag_id_seq', 1, false);
-
-
---
--- Name: course_users course_users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY course_users
-    ADD CONSTRAINT course_users_pkey PRIMARY KEY (course_id, user_id);
-
-
---
--- Name: courses courses_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY courses
-    ADD CONSTRAINT courses_pkey PRIMARY KEY (course_id);
-
-
---
--- Name: question_followers question_followers_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_followers
-    ADD CONSTRAINT question_followers_pkey PRIMARY KEY (question_id, follower);
-
-
---
--- Name: question_tags question_tags_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_tags
-    ADD CONSTRAINT question_tags_pkey PRIMARY KEY (tag_id, question_id);
-
-
---
--- Name: questions questions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY questions
-    ADD CONSTRAINT questions_pkey PRIMARY KEY (question_id);
-
-
---
--- Name: session_tas session_tas_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY session_tas
-    ADD CONSTRAINT session_tas_pkey PRIMARY KEY (session_id, ta);
-
-
---
--- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY sessions
-    ADD CONSTRAINT sessions_pkey PRIMARY KEY (session_id);
-
-
---
--- Name: tags tags_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY tags
-    ADD CONSTRAINT tags_pkey PRIMARY KEY (tag_id);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (netid);
-
-
---
--- Name: course_users course_users_course_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY course_users
-    ADD CONSTRAINT course_users_course_id_fkey FOREIGN KEY (course_id) REFERENCES courses(course_id);
-
-
---
--- Name: course_users course_users_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY course_users
-    ADD CONSTRAINT course_users_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(netid);
-
-
---
--- Name: question_followers question_followers_follower_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_followers
-    ADD CONSTRAINT question_followers_follower_fkey FOREIGN KEY (follower) REFERENCES users(netid);
-
-
---
--- Name: question_followers question_followers_question_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_followers
-    ADD CONSTRAINT question_followers_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions(question_id);
-
-
---
--- Name: question_tags question_tags_question_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_tags
-    ADD CONSTRAINT question_tags_question_id_fkey FOREIGN KEY (question_id) REFERENCES questions(question_id);
-
-
---
--- Name: question_tags question_tags_tag_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY question_tags
-    ADD CONSTRAINT question_tags_tag_id_fkey FOREIGN KEY (tag_id) REFERENCES tags(tag_id);
-
-
---
--- Name: questions questions_session_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY questions
-    ADD CONSTRAINT questions_session_id_fkey FOREIGN KEY (session_id) REFERENCES sessions(session_id);
-
-
---
--- Name: questions questions_student_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY questions
-    ADD CONSTRAINT questions_student_fkey FOREIGN KEY (student) REFERENCES users(netid);
-
-
---
--- Name: session_tas session_tas_session_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY session_tas
-    ADD CONSTRAINT session_tas_session_id_fkey FOREIGN KEY (session_id) REFERENCES sessions(session_id);
-
-
---
--- Name: session_tas session_tas_ta_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY session_tas
-    ADD CONSTRAINT session_tas_ta_fkey FOREIGN KEY (ta) REFERENCES users(netid);
-
-
---
--- Name: sessions sessions_course_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY sessions
-    ADD CONSTRAINT sessions_course_id_fkey FOREIGN KEY (course_id) REFERENCES courses(course_id);
-
-
---
--- Name: tags tags_course_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY tags
-    ADD CONSTRAINT tags_course_id_fkey FOREIGN KEY (course_id) REFERENCES courses(course_id);
-
-
---
--- PostgreSQL database dump complete
---
+ALTER TABLE "sessionSeriesTas" ADD CONSTRAINT "sessionSeriesTas_fk0" FOREIGN KEY ("sessionSeriesId") REFERENCES "sessionSeries"("sessionSeriesId");
+ALTER TABLE "sessionSeriesTas" ADD CONSTRAINT "sessionSeriesTas_fk1" FOREIGN KEY ("userId") REFERENCES "users"("userId");
 
