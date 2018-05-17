57b637c018a96152ec7b2f8d25d8676e5238e5ad
Ryan Slama <rockinryguy@gmail.com>
2018-05-17 13:41:46 -0400

    Large bugfix - Join Queue button

--- client/src/components/includes/SessionQuestionsContainer.tsx
+++ client/src/components/includes/SessionQuestionsContainer.tsx
@@ -12,7 +12,7 @@ class SessionQuestionsContainer extends React.Component {
 
     render() {
         var questions = this.props.questions;
-        var userQuestionIndex: number = 0;
+        var userQuestionIndex: number = -1;
         var cardList: JSX.Element[] = [];
         questions.forEach((question, i: number) => {
             // TODO: change before shipping
@@ -40,7 +40,7 @@ class SessionQuestionsContainer extends React.Component {
 
             <div className="SessionQuestionsContainer" >
                 {!this.props.isTA && userQuestionIndex === -1 &&
-                    <div className="SessionJoinButton" onClick={() => this.props.handleJoinClick}>
+                    <div className="SessionJoinButton" onClick={() => this.props.handleJoinClick()}>
                         <p><Icon name="plus" /> Join the Queue</p>
                     </div>
                 }
