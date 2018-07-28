6ab98aca00b0e6137f3f308439d0b7c337ee0aa0
Ryan Slama <rockinryguy@gmail.com>
2018-07-27 21:55:25 -0700

    Working log out and send feedback

--- client/src/components/includes/CalendarHeader.tsx
+++ client/src/components/includes/CalendarHeader.tsx
@@ -1,11 +1,24 @@
 import * as React from 'react';
 
-class CalendarHeader extends React.PureComponent {
+class CalendarHeader extends React.Component {
     props: {
         currentCourseCode: string;
         isTa: boolean;
         avatar: string | null;
     };
+
+    state: {
+        showMenu: boolean;
+    };
+
+    constructor(props: {}) {
+        super(props);
+        this.state = { showMenu: false };
+    }
+
+    setMenu = (status: boolean) => {
+        this.setState({ showMenu: status });
+    }
 
     render() {
         return (
@@ -13,11 +26,26 @@ class CalendarHeader extends React.PureComponent {
                 <div className="CurrentCourse">
                     <span>{this.props.currentCourseCode}</span>
                     {this.props.isTa && <span className="TAMarker">TA</span>}
-                    {this.props.avatar && <img className="mobileHeaderFace" src={this.props.avatar} />}
-                    {/* <button className="CourseSelectButton">
-                        <i className="angle down icon" />
-                    </button> */}
+                    {this.props.avatar &&
+                        <img
+                            className="mobileHeaderFace"
+                            onClick={() => this.setMenu(!this.state.showMenu)}
+                            src={this.props.avatar}
+                        />
+                    }
                 </div>
+                {this.state.showMenu && (
+                    <ul className="mobileLogoutMenu" onClick={() => this.setMenu(false)} >
+                        {/* {this.props.isTa &&
+                            <React.Fragment>
+                                <li>Cancel Session</li>
+                                <li>Change Session</li>
+                            </React.Fragment>
+                        } */}
+                        <li><a href="/__auth/logout">Log Out</a></li>
+                        <li><a href="https://goo.gl/forms/7ozmsHfXYWNs8Y2i1">Send Feedback</a></li>
+                    </ul>
+                )}
             </div>
         );
     }
