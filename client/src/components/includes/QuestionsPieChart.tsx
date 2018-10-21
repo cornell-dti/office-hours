162db38b8f2fcd51da230b4bc9e52c33e95d97c6
Rachel Susanne Nash <rsn55@cornell.edu>
2018-10-21 02:38:46 -0400

    finished basic functionality of pie chart and the other stats that do not involve graphs

--- client/src/components/includes/QuestionsPieChart.tsx
+++ client/src/components/includes/QuestionsPieChart.tsx
@@ -1,40 +1,5 @@
 import * as React from 'react';
-// import gql from 'graphql-tag';
-// import { Query } from 'react-apollo';
 import { ResponsivePie, PieDatum } from '@nivo/pie';
-
-// const METADATA_QUERY = gql`
-// query GetMetadata($courseId: Int!) {
-//     apiGetCurrentUser {
-//         nodes {
-//             computedName
-//             computedAvatar
-//             courseUsersByUserId(condition:{courseId:$courseId}) {
-//                 nodes {
-//                     role
-//                 }
-//             }
-//         }
-//     }
-//     courseByCourseId(courseId: $courseId) {
-//         code
-//     }
-// }`;
-
-// interface ProfessorMetadataData {
-//     apiGetCurrentUser: {
-//         nodes: [AppUserRole]
-//     };
-//     courseByCourseId: {
-//         code: string
-//     };
-// }
-
-// interface MetadataVariables {
-//     courseId: number;
-// }
-
-// class ProfessorMetadataDataQuery extends Query<ProfessorMetadataData, MetadataVariables> { }
 
 class QuestionsPieChart extends React.Component {
   props: {
@@ -49,13 +14,13 @@ class QuestionsPieChart extends React.Component {
   constructor(props: {
       percentResolved: number,
       percentUnresolved: number
-    } ) {
+      } ) {
       super(props);
       this.state = {
         data: [
                 {
                   'id': 'answered',
-                  'value': this.props.percentResolved,
+                   'value': this.props.percentResolved,
                   'key': 'answer'
                 },
                 {
@@ -72,12 +37,25 @@ render() {
           <div className="QuestionsPieChart" style={{ height: 400 }}>
 
                             <ResponsivePie
-                              data={this.state.data}
+                              data={(this.state = {
+                                data: [
+                                        {
+                                          'id': 'answered',
+                                           'value': this.props.percentResolved,
+                                          'key': 'answer'
+                                        },
+                                        {
+                                          'id': 'unanswered',
+                                          'value': this.props.percentUnresolved,
+                                          'key': 'no-answer'
+                                        }
+                                      ] as PieDatum[]
+                              }, this.state.data)}
                               margin={{
-                                  'top': 40,
-                                  'right': 80,
-                                  'bottom': 80,
-                                  'left': 80
+                                  'top': 5,
+                                  'right': 40,
+                                  'bottom': 10,
+                                  'left': 40
                               }}
                               innerRadius={0.5}
                               padAngle={0.7}
