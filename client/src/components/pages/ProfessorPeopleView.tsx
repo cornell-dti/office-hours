bd37c35406dda3bdf95919a3e1dc5479f785ec36
Rachel Susanne Nash <rsn55@cornell.edu>
2018-10-21 02:38:46 -0400

    finished basic functionality of pie chart and the other stats that do not involve graphs

--- client/src/components/pages/ProfessorPeopleView.tsx
+++ client/src/components/pages/ProfessorPeopleView.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 // import TopBar from '../includes/TopBar';
 import ProfessorSidebar from '../includes/ProfessorSidebar';
 import QuestionsPieChart from '../includes/QuestionsPieChart';
+// import QuestionsLineChart from '../includes/QuestionsLineChart';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import { Redirect } from 'react-router';
@@ -34,14 +35,28 @@ query GetMetadata($courseId: Int!, $startDate: Datetime!, $endDate: Datetime!) {
       _endTime: $endDate,
       _courseId: $courseId
     ){
-        nodes{
-          questionsBySessionId{
-            nodes{
-              status
-            }
+      nodes{
+        sessionId
+        startTime
+        endTime
+        building
+        room
+        questionsBySessionId{
+          nodes{
+            status
           }
         }
-      }
+        sessionTasBySessionId{
+         nodes{
+            userByUserId{
+              userId
+            }
+         }
+       }
+
+     }
+
+ }
 }`;
 
 interface ProfessorMetadataData {
@@ -103,25 +118,72 @@ class ProfessorPeopleView extends React.Component {
                       var courseCode: string = 'Loading...';
                       var resolvedQuestions: number = 0;
                       var unresolvedQuestions: number = 0;
-                      var percentResolved: number = 0;
-                      var percentUnresolved: number = 0;
+                      let percentResolved: number = 0;
+                      let percentUnresolved: number = 0;
+                      let totalQuestions: number = 0;
+                      let questionsInBusiestSession: number = 0;
+                      let questionsByDate: {date: string, questions: number}[] = [];
+                      let mostCrowdedDay: string = '';
+                      let busiestSessionInfo: {
+                        taId: number,
+                        firstName: string,
+                        lastName: string,
+                        startTime: string,
+                        endTime: string,
+                        building: string,
+                        room: string
+                      } = {
+                        taId: 0,
+                        firstName: '',
+                        lastName: '',
+                        startTime: '',
+                        endTime: '',
+                        building: '',
+                        room: ''
+                      };
                       if (!loading && data) {
                           courseCode = data.courseByCourseId.code;
                           if (data.apiGetCurrentUser.nodes[0].courseUsersByUserId.nodes[0].role !== 'professor') {
                               return <Redirect to={'/course/' + this.props.match.params.courseId} />;
                           }
                           data.apiGetSessions.nodes.forEach((n) => {
+                            let questionsInThisSession = 0;
                             n.questionsBySessionId.nodes.forEach((q) => {
+                              totalQuestions++;
+                              questionsInThisSession++;
                               if (q.status === 'resolved' || q.status === 'retracted') {
                                 resolvedQuestions++;
                               } else if (q.status === 'unresolved') {
                                 unresolvedQuestions++;
                               }
                             });
+                            if (questionsInThisSession >= questionsInBusiestSession) {
+                              questionsInBusiestSession = questionsInThisSession;
+                              busiestSessionInfo.taId = n.sessionTasBySessionId.nodes[0].userByUserId.userId;
+                              busiestSessionInfo.firstName = '';
+                              busiestSessionInfo.lastName = '';
+                              busiestSessionInfo.startTime = moment(n.startTime).format('MMMM Do YYYY, h:mm a');
+                              busiestSessionInfo.endTime = moment(n.endTime).format('h:mm a');
+                              busiestSessionInfo.building = n.building;
+                              busiestSessionInfo.room = n.room;
+                            }
+                            let dateString = moment(n.startTime).format('MMMM Do YYYY');
+                            if (dateString in questionsByDate) {
+                              questionsByDate[dateString].questions += questionsInThisSession;
+                            } else {
+                              questionsByDate.push({date: dateString, questions: questionsInThisSession});
+                            }
                           });
                           percentResolved = Math.round((resolvedQuestions /
                           (resolvedQuestions + unresolvedQuestions)) * 100);
                           percentUnresolved = 100 - percentResolved;
+                          let questionsOfBusiestDay = 0;
+                          questionsByDate.forEach((d) => {
+                            if (d.questions >= questionsOfBusiestDay) {
+                              mostCrowdedDay = d.date;
+                              questionsOfBusiestDay = d.questions;
+                            }
+                          });
                       }
                       return (
                           <React.Fragment>
@@ -151,11 +213,28 @@ class ProfessorPeopleView extends React.Component {
                                 onFocusChange={focusedInput => this.setState({ focusedInput })}
                               />
                             </div>
-                            <div style={{ height: '50em', width: '50em' }}>
+                            <div className="Total-Questions-Box">
                                 <QuestionsPieChart
                                     percentResolved={percentResolved}
                                     percentUnresolved={percentUnresolved}
                                 />
+                                <div>
+                                  <p> <span className="Question-Number">{totalQuestions} </span>
+                                  <br/> questions total</p>
+                                </div>
+                            </div>
+                            <div className="Most-Crowded-Box">
+                                <div>
+                                  <p> Most Crowded Day </p>
+                                  <p> {mostCrowdedDay} </p>
+                                </div>
+                                <hr/>
+                                <div>
+                                  <p> Most Crowded Office Hour </p>
+                                  <p> {busiestSessionInfo.startTime} - {busiestSessionInfo.endTime} </p>
+                                  <p> {busiestSessionInfo.building} {busiestSessionInfo.room} </p>
+                                </div>
+                                {/* <QuestionsLineChart /> */}
                             </div>
                           </React.Fragment>
                       );
