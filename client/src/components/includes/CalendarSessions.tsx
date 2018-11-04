ddd64c7dbcac77f5fd0bd7cca44d4924c77aadab
vanessaw1 <wang.vanessa1@gmail.com>
2018-11-04 14:50:35 -0500

    Add bookmark to calendar view

--- client/src/components/includes/CalendarSessions.tsx
+++ client/src/components/includes/CalendarSessions.tsx
@@ -8,6 +8,7 @@ import CalendarSessionCard from './CalendarSessionCard';
 class CalendarSessions extends React.PureComponent {
     props: {
         activeSessionId: number;
+        myUserId: number;
         loading: boolean;
         sessions: AppSession[] | null;
         callback: Function;
@@ -29,15 +30,29 @@ class CalendarSessions extends React.PureComponent {
         const loading = this.props.loading;
         const sessions = this.props.sessions;
 
-        const sessionCards = sessions && sessions.map(session => (
-            <CalendarSessionCard
-                session={session}
-                key={session.sessionId}
-                callback={this.props.callback}
-                active={session.sessionId === this.props.activeSessionId}
-                status={this.labelSession(session, Interval.toMillisecoonds(this.props.interval))}
-            />
-        ));
+        const sessionCards = sessions && sessions.map(session => {
+            const sessionAskerIds = session.questionsBySessionId.nodes.map(question => (
+                question.userByAskerId && question.userByAskerId.userId
+            ));
+            const sessionStatuses = session.questionsBySessionId.nodes.map(question => (
+                question.status
+            ));
+
+            const userQuestionIndex = sessionAskerIds.lastIndexOf(this.props.myUserId);
+            const userInQueue = userQuestionIndex === -1 ? false
+                : sessionStatuses[userQuestionIndex] === 'unresolved';
+
+            return (
+                <CalendarSessionCard
+                    includeBookmark={userInQueue}
+                    session={session}
+                    key={session.sessionId}
+                    callback={this.props.callback}
+                    active={session.sessionId === this.props.activeSessionId}
+                    status={this.labelSession(session, Interval.toMillisecoonds(this.props.interval))}
+                />
+            );
+        });
 
         const groupedCards = sessionCards && groupBy(sessionCards, (card: CalendarSessionCard) => card.props.status);
 
