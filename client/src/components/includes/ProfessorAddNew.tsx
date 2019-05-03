1ea38e0c9da91618a17e1fe038a645d3319223d1
Ryan Slama <rockinryguy@gmail.com>
2019-05-03 18:39:12 -0400

    More refactoring

--- client/src/components/includes/ProfessorAddNew.tsx
+++ client/src/components/includes/ProfessorAddNew.tsx
@@ -1,74 +1,55 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import { Icon, DropdownItemProps } from 'semantic-ui-react';
-import ProfessorOHInfo from '../includes/ProfessorOHInfo';
+import ProfessorOHInfo from './ProfessorOHInfo';
 import ProfessorTagInfo from './ProfessorTagInfo';
 import 'react-datepicker/dist/react-datepicker.css';
 
-class ProfessorAddNew extends React.Component {
-
+const ProfessorAddNew = (
     props: {
-        courseId: string
-        refreshCallback: Function
-        taOptions?: DropdownItemProps[]
-    };
+        courseId: string;
+        refreshCallback: Function;
+        taOptions?: DropdownItemProps[];
+    },
+) => {
+    const [editVisible, setEditVisible] = useState(false);
 
-    state: {
-        editVisible: false
-    };
+    const text = props.taOptions ? 'Add New Office Hour' : 'Add New Assignment';
 
-    constructor(props: {}) {
-        super(props);
-        this.state = {
-            editVisible: false
-        };
-    }
-
-    toggleEdit(toggle: boolean) {
-        this.setState({
-            editVisible: toggle
-        });
-    }
-
-    render() {
-
-        var text = this.props.taOptions ? 'Add New Office Hour' : 'Add New Assignment';
-
-        return (
-            <div className="ProfessorAddNew">
-                <div className={'Add ' + !this.state.editVisible}>
-                    <button className="NewOHButton" onClick={() => this.toggleEdit(true)}>
+    return (
+        <div className="ProfessorAddNew">
+            <div className={`Add ${!editVisible}`}>
+                <button className="NewOHButton" type="button" onClick={() => setEditVisible(true)}>
+                    <Icon name="plus" />
+                    {text}
+                </button>
+            </div>
+            <div className={`ExpandedAdd ${editVisible}`}>
+                <div className="NewOHHeader">
+                    <button className="ExpandedNewOHButton" type="submit" onClick={() => setEditVisible(false)}>
                         <Icon name="plus" />
                         {text}
                     </button>
                 </div>
-                <div className={'ExpandedAdd ' + this.state.editVisible}>
-                    <div className="NewOHHeader">
-                        <button className="ExpandedNewOHButton" onClick={() => this.toggleEdit(false)}>
-                            <Icon name="plus" />
-                            {text}
-                        </button>
-                    </div>
-                    {this.props.taOptions ?
-                        <ProfessorOHInfo
-                            courseId={this.props.courseId}
-                            isNewOH={true}
-                            taOptions={this.props.taOptions}
-                            toggleEdit={() => this.toggleEdit(false)}
-                            taUserIdsDefault={[]}
-                            refreshCallback={this.props.refreshCallback}
-                        />
-                        :
+                {props.taOptions
+                    ? <ProfessorOHInfo
+                        courseId={props.courseId}
+                        isNewOH
+                        taOptions={props.taOptions}
+                        toggleEdit={() => setEditVisible(false)}
+                        taUserIdsDefault={[]}
+                        refreshCallback={props.refreshCallback}
+                    /> : (
                         <ProfessorTagInfo
-                            isNew={true}
-                            cancelCallback={() => this.toggleEdit(false)}
-                            refreshCallback={this.props.refreshCallback}
-                            courseId={this.props.courseId}
+                            isNew
+                            cancelCallback={() => setEditVisible(false)}
+                            refreshCallback={props.refreshCallback}
+                            courseId={props.courseId}
                         />
-                    }
-                </div>
+                    )
+                }
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default ProfessorAddNew;
--- client/src/components/includes/ProfessorCalendarRow.tsx
+++ client/src/components/includes/ProfessorCalendarRow.tsx
@@ -2,116 +2,113 @@ import * as React from 'react';
 import * as moment from 'moment';
 import { Icon, DropdownItemProps } from 'semantic-ui-react';
 import 'react-datepicker/dist/react-datepicker.css';
+import Moment from 'react-moment';
 import ProfessorOHInfo from './ProfessorOHInfo';
 
-class ProfessorCalendarRow extends React.Component {
 
-    props: {
-        dayNumber: number,
-        sessions: AppSession[],
-        courseId: string,
-        taOptions: DropdownItemProps[],
-        isExpanded: boolean[],
-        handleEditToggle: Function,
-        updateDeleteInfo: Function,
-        updateDeleteVisible: Function,
-        refreshCallback: Function
+const ProfessorCalendarRow = (props: {
+    dayNumber: number;
+    sessions: FireSession[];
+    courseId: string;
+    taOptions: DropdownItemProps[];
+    isExpanded: boolean[];
+    handleEditToggle: Function;
+    updateDeleteInfo: Function;
+    updateDeleteVisible: Function;
+    refreshCallback: Function;
+}) => {
+    const toggleEdit = (row: number) => {
+        props.handleEditToggle(props.dayNumber, row);
     };
 
-    constructor(props: {}) {
-        super(props);
-        this.toggleEdit = this.toggleEdit.bind(this);
-        this.updateDeleteInfo = this.updateDeleteInfo.bind(this);
-    }
+    const updateDeleteInfo = (dayIndex: number, rowIndex: number) => {
+        props.updateDeleteInfo(dayIndex, rowIndex);
+        props.updateDeleteVisible(true);
+    };
 
-    toggleEdit(row: number) {
-        this.props.handleEditToggle(this.props.dayNumber, row);
-    }
-
-    updateDeleteInfo(dayIndex: number, rowIndex: number) {
-        this.props.updateDeleteInfo(dayIndex, rowIndex);
-        this.props.updateDeleteVisible(true);
-    }
-
-    render() {
-        if (this.props.sessions.length === 0) {
-            return (
-                <tbody>
-                    <tr>
-                        <td colSpan={5} className="NoOH">
-                            <i>No office hours scheduled</i>
-                        </td>
-                    </tr>
-                </tbody>
-            );
-        }
-
-        var rowPair = this.props.sessions.map(
-            (row, i) => {
-                return (
-                    <tbody
-                        className={'Pair ' + this.props.isExpanded[i] + ' ' + (i % 2 === 0 ? 'odd' : 'even')}
-                        key={this.props.sessions[i].sessionId}
-                    >
-                        <tr className="Preview">
-                            <td>
-                                {moment(this.props.sessions[i].startTime).format('h:mm A')}
-                                {' to '}
-                                {moment(this.props.sessions[i].endTime).format('h:mm A')}
-                            </td>
-                            <td>
-                                {this.props.sessions[i].sessionTasBySessionId.nodes
-                                    .map(ta => ta.userByUserId.computedName).join(', ')}
-                            </td>
-                            <td>{this.props.sessions[i].building} {this.props.sessions[i].room}</td>
-                            <td>
-                                <button
-                                    className="Edit"
-                                    onClick={() => this.toggleEdit(i)}
-                                >
-                                    <Icon name="pencil" />
-                                </button>
-                            </td>
-                            <td>
-                                <button
-                                    className="Delete"
-                                    onClick={() => this.updateDeleteInfo(this.props.dayNumber, i)}
-                                >
-                                    <Icon name="x" />
-                                </button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td
-                                colSpan={5}
-                                className={'ExpandedEdit ' + this.props.isExpanded[i]}
-                            >
-                                <ProfessorOHInfo
-                                    key={this.props.sessions[i].sessionId}
-                                    session={this.props.sessions[i]}
-                                    courseId={this.props.courseId}
-                                    isNewOH={false}
-                                    taOptions={this.props.taOptions}
-                                    toggleEdit={() => this.toggleEdit(i)}
-                                    refreshCallback={this.props.refreshCallback}
-                                />
-                                <button
-                                    className="Bottom Delete"
-                                    onClick={() => this.updateDeleteInfo(this.props.dayNumber, i)}
-                                >
-                                    Delete
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody >
-                );
-            }
-        );
-
+    if (props.sessions.length === 0) {
         return (
-            rowPair
+            <tbody>
+                <tr>
+                    <td colSpan={5} className="NoOH">
+                        <i>No office hours scheduled</i>
+                    </td>
+                </tr>
+            </tbody>
         );
     }
-}
+
+    const rowPair = props.sessions.map(
+        (row, i) => (
+            <tbody
+                className={`Pair ${props.isExpanded[i]} ${i % 2 === 0 ? 'odd' : 'even'}`}
+                key={props.sessions[i].id}
+            >
+                <tr className="Preview">
+                    <td>
+                        <Moment date={props.sessions[i].startTime.seconds} unix interval={0} format="hh:mm A" />
+                        {' to '}
+                        <Moment date={props.sessions[i].endTime.seconds} unix interval={0} format="hh:mm A" />
+                    </td>
+                    <td>
+                        {/* TODO */}
+                        {/* {props.sessions[i].sessionTasBySessionId.nodes
+                            .map(ta => ta.userByUserId.computedName).join(', ')} */}
+                    </td>
+                    <td>
+                        {props.sessions[i].building}
+                        {' '}
+                        {props.sessions[i].room}
+                    </td>
+                    <td>
+                        <button
+                            className="Edit"
+                            onClick={() => toggleEdit(i)}
+                            type="button"
+                        >
+                            <Icon name="pencil" />
+                        </button>
+                    </td>
+                    <td>
+                        <button
+                            className="Delete"
+                            onClick={() => updateDeleteInfo(props.dayNumber, i)}
+                            type="button"
+                        >
+                            <Icon name="x" />
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td
+                        colSpan={5}
+                        className={`ExpandedEdit ${props.isExpanded[i]}`}
+                    >
+                        <ProfessorOHInfo
+                            key={props.sessions[i].id}
+                            session={props.sessions[i]}
+                            courseId={props.courseId}
+                            isNewOH={false}
+                            taOptions={props.taOptions}
+                            toggleEdit={() => toggleEdit(i)}
+                            refreshCallback={props.refreshCallback}
+                        />
+                        <button
+                            className="Bottom Delete"
+                            onClick={() => updateDeleteInfo(props.dayNumber, i)}
+                            type="button"
+                        >
+                            Delete
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        ),
+    );
+
+    return (
+        rowPair
+    );
+};
 
 export default ProfessorCalendarRow;
