ac1ff876c097c07c0191422604de9b82191c82ab
Bobby Villaluz <30696102+BobbyVillaluz@users.noreply.github.com>
2019-10-16 12:18:50 -0400

    Disable cornell.edu check temporarily (#152)
    * Disable cornell.edu check temporarily
    
    * Minor change to trigger redeploy

--- server/index.ts
+++ server/index.ts
@@ -138,7 +138,7 @@ app.get('/__auth',
     passport.authenticate('google', {
         scope: ['profile', 'email'],
         // @ts-ignore: Hosted domain is used by the Google strategy, but not allowed in passport's types
-        hostedDomain: "cornell.edu",
+        // TODO: ** !DO NOT DEPLOY TO PRODUCTION! temporarily disabled for Kevin Kidwell ** hostedDomain: "cornell.edu",
         failureRedirect: '/login'
     })
 )
