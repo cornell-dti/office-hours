41e82313064301d5170088330b30c9f43c44ece2
Sam Zhou <sam@developersam.com>
2020-03-20 14:15:56 -0400

    Fix some linter warnings (#246)

--- client/src/components/includes/SessionView.tsx
+++ client/src/components/includes/SessionView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 import TopBar from '../includes/TopBar';
 import SessionInformationHeader from '../includes/SessionInformationHeader';
