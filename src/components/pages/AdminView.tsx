646a12b674f71a9bc6e77202358140452c2d5f19
Sam <sam@developersam.com>
2020-04-02 22:04:01 -0400

    Support creating and editing some fields of an existing course in admin page
    ### Summary
    
    This diff allows user to edit certain fields of an existing class. Fields that cannot be edited are the fields that can be edited on the professor side, or start and end date.
    
    I also updated the firebase security rule that allows an admin user to edit course. Right now, I hardcoded the emails of Scott and I in that rule.
    
    ### Test Plan
    
    Try to edit the dummy CS 3110 class or create a new class. The UX is not good but it works.

--- src/components/pages/AdminView.tsx
+++ src/components/pages/AdminView.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import * as firebase from 'firebase/app';
 
 import { useAllCourses, useCourseProfessorMap, useCourseTAMap } from '../../firehooks';
+import { firestore } from '../../firebase';
 
-const AdminCourseCard = ({ course }: { readonly course: FireCourse }) => {
+const AdminReadOnlyCourseCard = ({ course }: { readonly course: FireCourse }) => {
     const professorMap = useCourseProfessorMap(course);
     const taMap = useCourseTAMap(course);
 
     return (
-        <div className="course">
+        <div>
             <div className="course-section">
                 <h3>Course ID</h3>
                 <div>{course.courseId}</div>
@@ -23,6 +25,10 @@ const AdminCourseCard = ({ course }: { readonly course: FireCourse }) => {
             <div className="course-section">
                 <h3>Semester</h3>
                 <div>{course.semester}</div>
+            </div>
+            <div className="course-section">
+                <h3>Year</h3>
+                <div>{course.year}</div>
             </div>
             <div className="course-section">
                 <h3>Term</h3>
@@ -82,15 +88,143 @@ const AdminCourseCard = ({ course }: { readonly course: FireCourse }) => {
     );
 };
 
+const AdminEditableCourseCard = ({ course }: { readonly course: FireCourse }) => {
+    const [name, setName] = useState(course.name);
+    const [code, setCode] = useState(course.code);
+    const [semester, setSemester] = useState(course.semester);
+    const [year, setYear] = useState(course.year);
+    const [term, setTerm] = useState(course.term);
+
+    const onSave = () => {
+        const update: Partial<FireCourse> = { name, code, semester, year, term };
+        firestore.collection('courses').doc(course.courseId).update(update);
+    };
+
+    return (
+        <div>
+            <div className="course-section">
+                <h3>Course ID</h3>
+                <div>{course.courseId}</div>
+            </div>
+            <div className="course-section">
+                <h3>Course Name</h3>
+                <input type="text" value={name} onChange={e => setName(e.currentTarget.value)} />
+            </div>
+            <div className="course-section">
+                <h3>Course Code</h3>
+                <input type="text" value={code} onChange={e => setCode(e.currentTarget.value)} />
+            </div>
+            <div className="course-section">
+                <h3>Semester</h3>
+                <input type="text" value={semester} onChange={e => setSemester(e.currentTarget.value)} />
+            </div>
+            <div className="course-section">
+                <h3>Year</h3>
+                <input type="text" value={year} onChange={e => setYear(e.currentTarget.value)} />
+            </div>
+            <div className="course-section">
+                <h3>Term</h3>
+                <input type="text" value={term} onChange={e => setTerm(e.currentTarget.value)} />
+            </div>
+            <button onClick={onSave}>Save</button>
+        </div>
+    );
+};
+
+const AdminCourseCard = ({ course }: { readonly course: FireCourse }) => {
+    const [isEditingMode, setIsEditingMode] = useState(false);
+    return (
+        <div className="course">
+            {!isEditingMode && <AdminReadOnlyCourseCard course={course} />}
+            {isEditingMode && <AdminEditableCourseCard course={course} />}
+            <button onClick={() => setIsEditingMode(prev => !prev)}>
+                To {isEditingMode ? 'Read Only' : 'Editing'} Mode
+            </button>
+        </div>
+    );
+};
+
+
+const startDate = new Date('2020-03-30');
+const endDate = new Date('2020-06-30');
+
+const AdminCourseCreator = ({ onSubmit }: { readonly onSubmit: () => void }) => {
+    const [name, setName] = useState('');
+    const [code, setCode] = useState('');
+    const [semester, setSemester] = useState('');
+    const [year, setYear] = useState('');
+    const [term, setTerm] = useState('');
+
+    const onSave = () => {
+        const course: Omit<FireCourse, 'courseId'> = {
+            name,
+            code,
+            semester,
+            year,
+            term,
+            queueOpenInterval: 30,
+            charLimit: 140,
+            startDate: firebase.firestore.Timestamp.fromDate(startDate),
+            endDate: firebase.firestore.Timestamp.fromDate(endDate),
+            professors: [],
+            tas: []
+        };
+        firestore.collection('courses').add(course).then(onSubmit);
+    };
+
+    return (
+        <div>
+            <h2>Create New Course</h2>
+            <div className="course">
+                <div className="course-section">
+                    <h3>Course Name</h3>
+                    <input type="text" value={name} onChange={e => setName(e.currentTarget.value)} />
+                </div>
+                <div className="course-section">
+                    <h3>Course Code</h3>
+                    <input type="text" value={code} onChange={e => setCode(e.currentTarget.value)} />
+                </div>
+                <div className="course-section">
+                    <h3>Semester</h3>
+                    <input type="text" value={semester} onChange={e => setSemester(e.currentTarget.value)} />
+                </div>
+                <div className="course-section">
+                    <h3>Year</h3>
+                    <input type="text" value={year} onChange={e => setYear(e.currentTarget.value)} />
+                </div>
+                <div className="course-section">
+                    <h3>Term</h3>
+                    <input type="text" value={term} onChange={e => setTerm(e.currentTarget.value)} />
+                </div>
+                <div className="course-section">
+                    <h3>Start Date</h3>
+                    <div>{startDate.toLocaleDateString()}</div>
+                </div>
+                <div className="course-section">
+                    <h3>End Date</h3>
+                    <div>{endDate.toLocaleDateString()}</div>
+                </div>
+                <button onClick={onSave}>Save</button>
+            </div>
+        </div>
+    );
+};
+
 const AdminView = () => {
     const courses = useAllCourses();
+    const [inCreationMode, setInCreationMode] = useState(false);
 
     return (
         <div className="AdminView">
             <h2>Courses</h2>
-            {courses.map(course => (
-                <AdminCourseCard key={course.courseId} course={course} />
-            ))}
+            <div>
+                {courses.map(course => (
+                    <AdminCourseCard key={course.courseId} course={course} />
+                ))}
+            </div>
+            {inCreationMode
+                ? <AdminCourseCreator onSubmit={() => setInCreationMode(false)} />
+                : <button onClick={() => setInCreationMode(true)}>Create New Course</button>}
         </div>
     );
 };
