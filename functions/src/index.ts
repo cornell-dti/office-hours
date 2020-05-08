efcc9470000dc4e3e5aa383fd8bd8542848efce7
Guan <gqwu3000@gmail.com>
2020-05-08 23:13:17 +0800

    Made linter fixes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,33 +10,33 @@ import * as admin from 'firebase-admin';
 //  response.send("Hello from Firebase!");
 // });
 
-//Use admin SDK to enable writing to other parts of database
-//const admin = require('firebase-admin');
+// Use admin SDK to enable writing to other parts of database
+// const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
 
 exports.onQuestionCreate = functions.firestore
-.document('questions/{questionId}')
-.onCreate((snap, _ ) => {
-	//Get data object and obtain session ID
-	const data = snap.data();
-	const sessionId = data!.sessionId;
+    .document('questions/{questionId}')
+    .onCreate((snap) => {
+    // Get data object and obtain session ID
+    const data = snap.data();
+    const sessionId = data!.sessionId;
 
-    //Log Session ID for debugging
-	//console.log(`Session ID is: ${sessionId}`);
+    // Log Session ID for debugging
+    // console.log(`Session ID is: ${sessionId}`);
 
-	//Increment total number of questions of relevant session
+	// Increment total number of questions of relevant session
 	const increment = admin.firestore.FieldValue.increment(1);
 	return db.doc(`sessions/${sessionId}`).update({
 		totalQuestions: increment
 	});
 });
 
-//This map maps a question status to a tuple of
-//1. The number of questions this counts as (0 if no show or retracted)
-//2. The number of assigned questions this counts as (1 if assigned / resolved)
-//3. The number of resolved questions this counts as (1 if resolved only)
+// This map maps a question status to a tuple of
+// 1. The number of questions this counts as (0 if no show or retracted)
+// 2. The number of assigned questions this counts as (1 if assigned / resolved)
+// 3. The number of resolved questions this counts as (1 if resolved only)
 const questionStatusNumbers = new Map<string, number[]>();
 questionStatusNumbers.set("assigned", [1, 1, 0]);
 questionStatusNumbers.set("resolved", [1, 1, 1]);
@@ -45,8 +45,8 @@ questionStatusNumbers.set("unresolved", [1, 0, 0]);
 questionStatusNumbers.set("no-show", [0, 0, 0]);
 
 exports.onQuestionUpdate = functions.firestore
-.document('questions/{questionId}')
-.onUpdate((change, _ ) => {
+    .document('questions/{questionId}')
+    .onUpdate((change) => {
     const newQuestion = change.after.data()!;
     const prevQuestion = change.before.data()!;
 
@@ -67,17 +67,17 @@ exports.onQuestionUpdate = functions.firestore
     let waitTimeChange = 0;
     let resolveTimeChange = 0;
 
-    //Derive timing changes (changes from assigned to unassigned)
+    // Derive timing changes (changes from assigned to unassigned)
     if (numAssignedChange == 1){
-        //Add new time addressed
+        // Add new time addressed
         waitTimeChange = newQuestion.timeAssigned.seconds - newQuestion.timeEntered.seconds;
     }
     else if (numAssignedChange == -1){
-        //Subtract previous time addressed
+        // Subtract previous time addressed
         waitTimeChange = prevQuestion.timeEntered.seconds - prevQuestion.timeAssigned.seconds;
     }
 
-    //Derive timing changes (changes from assigned to resolved)
+    // Derive timing changes (changes from assigned to resolved)
     if (numResolvedChange == 1){
         resolveTimeChange = newQuestion.timeAddressed.seconds - newQuestion.timeAssigned.seconds;
     }
@@ -85,12 +85,12 @@ exports.onQuestionUpdate = functions.firestore
         resolveTimeChange = prevQuestion.timeAssigned.seconds - prevQuestion.timeAddressed.seconds;
     }
 
-    //Log for debugging
+    // Log for debugging
     console.log(`Status change from ${prevStatus} to ${newStatus}. Changes:
         ${numQuestionChange} ${numAssignedChange} ${numResolvedChange}
         ${waitTimeChange} ${resolveTimeChange}`);
 
-    //Update relevant statistics in database
+    // Update relevant statistics in database
     return db.doc(`sessions/${sessionId}`).update({
         totalQuestions: admin.firestore.FieldValue.increment(numQuestionChange),
         assignedQuestions: admin.firestore.FieldValue.increment(numAssignedChange),
