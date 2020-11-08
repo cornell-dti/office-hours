87c3696f5d0f890e7e3fae2e09d44864bf1c76fd
Guanqun Wu <gqwu3000@gmail.com>
2020-11-08 02:19:30 -0500

    Try to debug CI/CD environment

--- src/test/basic.test.tsx
+++ src/test/basic.test.tsx
@@ -16,8 +16,12 @@ describe('Unknown Question', function() {
     it('should not exist', async function() {
         // Expect question one to not exist
         const questionOne = firestore.collection('questions').doc("1").get();
+        console.log("Retrieving question");
         const result = await questionOne
+        console.log("Retrieved question");
 
         expect(result.exists).to.be.false;
+
+        return Promise.resolve();
     });
 });
