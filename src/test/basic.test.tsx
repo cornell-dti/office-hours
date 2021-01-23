19109ea65a20de79adcb623cd4566e66c57e9768
Scott Wang <curiosity142000@gmail.com>
2021-01-23 01:44:36 -0500

    commented out

--- src/test/basic.test.tsx
+++ src/test/basic.test.tsx
@@ -11,3 +11,18 @@ describe('Hello function', function() {
         expect(hello()).to.equal('Hello World!');
     });
 });
+
+// describe('Unknown Question', function() {
+//     it('should not exist', async function() {
+//         // Set a long timeout of 5s because it takes quite long on CI server
+//         this.timeout(5000);
+
+//         // Expect question one to not exist
+//         const questionOne = firestore.collection('questions').doc("1").get();
+//         const result = await questionOne
+
+//         expect(result.exists).to.be.false;
+
+//         return Promise.resolve();
+//     });
+// });
