0bc9b2a0762cfed7bdd8ee0fcba0e4a4724d942e
Nada Attia <nma46@cornell.edu>
2021-03-08 22:32:47 -0500

    Fix lint issues causing deploy check to fail (#506)
    * Fix warning
    
    * Fix leavequeue style
    
    * fix lint issues

--- src/components/includes/LeaveQueue.tsx
+++ src/components/includes/LeaveQueue.tsx
@@ -9,7 +9,7 @@ type Props = {
     removeQuestion: () => void;
 }
 
-const LeaveQueueModal = ({setShowModal, showModal, removeQuestion}: Props) => {
+const LeaveQueueModal = ({ setShowModal, showModal, removeQuestion }: Props) => {
 
     const handleYes = () => {
         removeQuestion();
@@ -19,35 +19,35 @@ const LeaveQueueModal = ({setShowModal, showModal, removeQuestion}: Props) => {
     return (
         <>
             {showModal &&
-        <div className="leave-queue-background">
-            <div className="leave-queue-container">
-                <button
-                    className="leave-queue-button"
-                    onClick={() => 
-                        setShowModal(false)}
-                    type="button"
-                >
-                    <img src={CloseIcon} alt="Close modal" />
-                </button>
-                <img src={AlertIcon} className="alert-icon" />
-                <h2>Are you sure you want to remove yourself from the queue?
-                </h2>
-                <button
-                    className="leave-queue-option yes-queue-button" 
-                    type="button"
-                    onClick={handleYes}
-                > 
-                    Yes
-                </button>
-                <button
-                    className="leave-queue-option cancel-queue-button" 
-                    type="button"
-                    onClick={() => setShowModal(false)}
-                >
-                    Cancel & Go Back
-                </button>
-            </div>
-        </div>
+                <div className="leave-queue-background">
+                    <div className="leave-queue-container">
+                        <button
+                            className="leave-queue-button"
+                            onClick={() =>
+                                setShowModal(false)}
+                            type="button"
+                        >
+                            <img src={CloseIcon} alt="Close modal" />
+                        </button>
+                        <img src={AlertIcon} className="alert-icon" alt="Alert" />
+                        <h2>Are you sure you want to remove yourself from the queue?
+                        </h2>
+                        <button
+                            className="leave-queue-option yes-queue-button"
+                            type="button"
+                            onClick={handleYes}
+                        >
+                            Yes
+                        </button>
+                        <button
+                            className="leave-queue-option cancel-queue-button"
+                            type="button"
+                            onClick={() => setShowModal(false)}
+                        >
+                            Cancel & Go Back
+                        </button>
+                    </div>
+                </div>
             }
         </>
     )
