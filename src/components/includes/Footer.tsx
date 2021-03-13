278033ae736cd2d52fa75e96dc227be7c70ba774
Jeremy Jung <jjchan921@gmail.com>
2021-03-13 11:38:20 -0600

    New landing page

---
+++ src/components/includes/Footer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="footerWrapper">
+            <div className="footerTextWrapper">
+                <p className="footerText">Made by</p>
+                <img className="footerLogo" />
+                <p className="footerText">{"Cornell Design \n& Tech Initiative"}</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
