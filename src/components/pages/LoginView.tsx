5f61109b5cdd586cc8ae109752f77c87d67b55a3
Alex Nagel <slipshot21@gmail.com>
2021-10-03 02:39:30 -0400

    adding environment check to @cornell.edu requirement

--- src/components/pages/LoginView.tsx
+++ src/components/pages/LoginView.tsx
@@ -23,155 +23,209 @@ import Analytics from "../../media/analytics.svg";
 import QMIThreePeople from "../../media/ppl_illustration.svg";
 
 const LoginView: React.FC = () => {
-  const history = useHistory();
+    const history = useHistory();
 
-  const auth = () => {
-    const authProvider = new firebase.auth.GoogleAuthProvider();
-    if (process.env.GCLOUD_PROJECT == "qmi") {
-      console.log(process.env.GCLOUD_PROJECT);
-      authProvider.setCustomParameters({
-        hd: "cornell.edu",
-      });
-    }
-    authProvider.addScope("email");
-    authProvider.addScope("profile");
+    const auth = () => {
+        const authProvider = new firebase.auth.GoogleAuthProvider();
+        if (
+            process.env.NODE_ENV === "production" &&
+            process.env.REACT_APP_IS_STAGING !== "true"
+        ) {
+            authProvider.setCustomParameters({
+                hd: "cornell.edu",
+            });
+        }
+        authProvider.addScope("email");
+        authProvider.addScope("profile");
 
-    return app
-      .auth()
-      .signInWithPopup(authProvider)
-      .then((response) => {
-        const user = response.user;
-        userUpload(user, firestore);
-        history.push("/");
-      });
-  };
+        return app
+            .auth()
+            .signInWithPopup(authProvider)
+            .then((response) => {
+                const user = response.user;
+                userUpload(user, firestore);
+                history.push("/");
+            });
+    };
 
-  const passAuth = () => {};
+    const passAuth = () => {};
 
-  return (
-    <div className="loginView">
-      <section className="header">
-        <div className="logoWrapper">
-          <img src={QMILogo2020} className="qmiNewLogo" alt="New QMI Logo" />
-          <h2 className="qmiLogoText">Queue Me In</h2>
-        </div>
-        <a
-          className="qmiContact"
-          href="mailto:queuemein@cornelldti.org?Subject=Queue%20Me%20In%20For%20Students"
-        >
-          Contact us
-        </a>
-      </section>
-      <section className="mainInfo">
-        <div className="nameAndButtonWrapper">
-          <h2 className="mainLogoText">Queue Me In</h2>
-          <h3 className="subHeader">Office Hours Simplified</h3>
-          <button type="button" className="loginButton" onClick={auth}>
-            <img src={googleLogo} className="googleLogo" alt="Google logo" />
-            <span className="loginButtonText">Sign in with Google</span>
-          </button>
-        </div>
-        <img src={QMIPeople} className="qmiPeople" alt="People lining up" />
-      </section>
-      <div className="demoWrapper">
-        <img src={Demo} className="demoQMI" alt="Demo of QMI" />
-        <div className="ribbonWrapper">
-          <img
-            src={WhiteStrip}
-            className="whiteStrip"
-            alt="Curved background strip"
-          />
-          <h2 className="sloganText">
-            {"Say goodbye to crowded and \nunorganized office hours."}
-          </h2>
-          <div className="bothTutorialsWrapper">
-            <div className="tutorialWrapper">
-              <h3 className="tutorialHeader">
-                {"For students who need \none-on-one support"}
-              </h3>
-              <div className="cardWrapper">
-                <div className="infoCard">
-                  <img src={Ask} className="cardImg" alt="Question bubble" />
-                  <p className="cardDesc">
-                    {"Ask questions in a \ndiscussion or queue"}
-                  </p>
+    return (
+        <div className="loginView">
+            <section className="header">
+                <div className="logoWrapper">
+                    <img
+                        src={QMILogo2020}
+                        className="qmiNewLogo"
+                        alt="New QMI Logo"
+                    />
+                    <h2 className="qmiLogoText">Queue Me In</h2>
                 </div>
-                <div className="infoCard">
-                  <img src={Reserve} className="cardImg" alt="Reserved" />
-                  <p className="cardDesc">
-                    {"Reserve time to speak \nwith a TA"}
-                  </p>
+                <a
+                    className="qmiContact"
+                    href="mailto:queuemein@cornelldti.org?Subject=Queue%20Me%20In%20For%20Students"
+                >
+                    Contact us
+                </a>
+            </section>
+            <section className="mainInfo">
+                <div className="nameAndButtonWrapper">
+                    <h2 className="mainLogoText">Queue Me In</h2>
+                    <h3 className="subHeader">Office Hours Simplified</h3>
+                    <button
+                        type="button"
+                        className="loginButton"
+                        onClick={auth}
+                    >
+                        <img
+                            src={googleLogo}
+                            className="googleLogo"
+                            alt="Google logo"
+                        />
+                        <span className="loginButtonText">
+                            Sign in with Google
+                        </span>
+                    </button>
                 </div>
-                <div className="infoCard">
-                  <img src={Wait} className="cardImg" alt="Waiting in line" />
-                  <p className="cardDesc">
-                    {"Get your questions \nanswered by course staff"}
-                  </p>
+                <img
+                    src={QMIPeople}
+                    className="qmiPeople"
+                    alt="People lining up"
+                />
+            </section>
+            <div className="demoWrapper">
+                <img src={Demo} className="demoQMI" alt="Demo of QMI" />
+                <div className="ribbonWrapper">
+                    <img
+                        src={WhiteStrip}
+                        className="whiteStrip"
+                        alt="Curved background strip"
+                    />
+                    <h2 className="sloganText">
+                        {
+                            "Say goodbye to crowded and \nunorganized office hours."
+                        }
+                    </h2>
+                    <div className="bothTutorialsWrapper">
+                        <div className="tutorialWrapper">
+                            <h3 className="tutorialHeader">
+                                {"For students who need \none-on-one support"}
+                            </h3>
+                            <div className="cardWrapper">
+                                <div className="infoCard">
+                                    <img
+                                        src={Ask}
+                                        className="cardImg"
+                                        alt="Question bubble"
+                                    />
+                                    <p className="cardDesc">
+                                        {
+                                            "Ask questions in a \ndiscussion or queue"
+                                        }
+                                    </p>
+                                </div>
+                                <div className="infoCard">
+                                    <img
+                                        src={Reserve}
+                                        className="cardImg"
+                                        alt="Reserved"
+                                    />
+                                    <p className="cardDesc">
+                                        {"Reserve time to speak \nwith a TA"}
+                                    </p>
+                                </div>
+                                <div className="infoCard">
+                                    <img
+                                        src={Wait}
+                                        className="cardImg"
+                                        alt="Waiting in line"
+                                    />
+                                    <p className="cardDesc">
+                                        {
+                                            "Get your questions \nanswered by course staff"
+                                        }
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="tutorialWrapper professorsTutorialWrapper">
+                            <h3 className="tutorialHeader">
+                                {
+                                    "For professors looking to shape \ntheir teaching experience"
+                                }
+                            </h3>
+                            <div className="cardWrapper">
+                                <div className="infoCard">
+                                    <img
+                                        src={Schedule}
+                                        className="cardImg"
+                                        alt="Calendar"
+                                    />
+                                    <p className="cardDesc">
+                                        {"Schedule your office \nhours easily"}
+                                    </p>
+                                </div>
+                                <div className="infoCard">
+                                    <img
+                                        src={Review}
+                                        className="cardImg"
+                                        alt="Asking and answering questions"
+                                    />
+                                    <p className="cardDesc">
+                                        {
+                                            "Assign TAs to discussions \nor host yourself"
+                                        }
+                                    </p>
+                                </div>
+                                <div className="infoCard">
+                                    <img
+                                        src={Analytics}
+                                        className="cardImg"
+                                        alt="Bar graph"
+                                    />
+                                    <p className="cardDesc">
+                                        {
+                                            "View office hour and \ndiscussion analytics"
+                                        }
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
+                <img
+                    src={SecondWhiteStrip}
+                    className="secondWhiteStrip"
+                    alt="Second curved background strip"
+                />
             </div>
-            <div className="tutorialWrapper professorsTutorialWrapper">
-              <h3 className="tutorialHeader">
-                {"For professors looking to shape \ntheir teaching experience"}
-              </h3>
-              <div className="cardWrapper">
-                <div className="infoCard">
-                  <img src={Schedule} className="cardImg" alt="Calendar" />
-                  <p className="cardDesc">
-                    {"Schedule your office \nhours easily"}
-                  </p>
-                </div>
-                <div className="infoCard">
-                  <img
-                    src={Review}
-                    className="cardImg"
-                    alt="Asking and answering questions"
-                  />
-                  <p className="cardDesc">
-                    {"Assign TAs to discussions \nor host yourself"}
-                  </p>
-                </div>
-                <div className="infoCard">
-                  <img src={Analytics} className="cardImg" alt="Bar graph" />
-                  <p className="cardDesc">
-                    {"View office hour and \ndiscussion analytics"}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <img
-          src={SecondWhiteStrip}
-          className="secondWhiteStrip"
-          alt="Second curved background strip"
-        />
-      </div>
 
-      <div className="whiteSectionWrapper">
-        <section className="finalTextSection">
-          <img
-            className="qmiThreePeople"
-            src={QMIThreePeople}
-            alt="People walking"
-          />
-          <div className="finalTextWrapper">
-            <h2 className="finalText finalTextHead">Simplify office hours.</h2>
-            <h2 className="finalText finalTextBody">
-              {"Manage the wave \nof students."}
-            </h2>
-            <a
-              className="qmiContact"
-              href="mailto:queuemein@cornelldti.org?Subject=Queue%20Me%20In%20For%20Students"
-            >
-              Contact us
-            </a>
-          </div>
-        </section>
-        <Footer />
-      </div>
-    </div>
-  );
+            <div className="whiteSectionWrapper">
+                <section className="finalTextSection">
+                    <img
+                        className="qmiThreePeople"
+                        src={QMIThreePeople}
+                        alt="People walking"
+                    />
+                    <div className="finalTextWrapper">
+                        <h2 className="finalText finalTextHead">
+                            Simplify office hours.
+                        </h2>
+                        <h2 className="finalText finalTextBody">
+                            {"Manage the wave \nof students."}
+                        </h2>
+                        <a
+                            className="qmiContact"
+                            href="mailto:queuemein@cornelldti.org?Subject=Queue%20Me%20In%20For%20Students"
+                        >
+                            Contact us
+                        </a>
+                    </div>
+                </section>
+                <Footer />
+            </div>
+        </div>
+    );
 };
 
 export default LoginView;
--- src/firebase.ts
+++ src/firebase.ts
@@ -1,31 +1,34 @@
-import 'firebase/auth';
-import 'firebase/firestore';
+import "firebase/auth";
+import "firebase/firestore";
 
-import firebase from 'firebase/app';
-import { authState } from 'rxfire/auth';
-import { collectionData } from 'rxfire/firestore';
-import { filter } from 'rxjs/operators';
+import firebase from "firebase/app";
+import { authState } from "rxfire/auth";
+import { collectionData } from "rxfire/firestore";
+import { filter } from "rxjs/operators";
 
 let firebaseConfig: object;
-if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_IS_STAGING !== 'true') {
+if (
+    process.env.NODE_ENV === "production" &&
+    process.env.REACT_APP_IS_STAGING !== "true"
+) {
     firebaseConfig = {
-        apiKey: 'AIzaSyBtxSkhR9RcnKP2FSsWtdxlwX4TcIjjm8A',
-        authDomain: 'queue-me-in-prod.firebaseapp.com',
-        databaseURL: 'https://queue-me-in-prod.firebaseio.com',
-        projectId: 'queue-me-in-prod',
-        storageBucket: 'queue-me-in-prod.appspot.com',
-        messagingSenderId: '283964683310',
-        appId: '1:283964683310:web:98ef1bd535c6315749dbbf',
-        measurementId: 'G-GHJ0TML275',
+        apiKey: "AIzaSyBtxSkhR9RcnKP2FSsWtdxlwX4TcIjjm8A",
+        authDomain: "queue-me-in-prod.firebaseapp.com",
+        databaseURL: "https://queue-me-in-prod.firebaseio.com",
+        projectId: "queue-me-in-prod",
+        storageBucket: "queue-me-in-prod.appspot.com",
+        messagingSenderId: "283964683310",
+        appId: "1:283964683310:web:98ef1bd535c6315749dbbf",
+        measurementId: "G-GHJ0TML275",
     };
 } else {
     firebaseConfig = {
-        apiKey: 'AIzaSyDD2hsUMX3qvOEotKBXzc1ehtMyunix_I4',
-        authDomain: 'qmi-test.firebaseapp.com',
-        databaseURL: 'https://qmi-test.firebaseio.com',
-        projectId: 'qmi-test',
-        storageBucket: 'qmi-test.appspot.com',
-        messagingSenderId: '349252319671',
+        apiKey: "AIzaSyDD2hsUMX3qvOEotKBXzc1ehtMyunix_I4",
+        authDomain: "qmi-test.firebaseapp.com",
+        databaseURL: "https://qmi-test.firebaseio.com",
+        projectId: "qmi-test",
+        storageBucket: "qmi-test.appspot.com",
+        messagingSenderId: "349252319671",
     };
 }
 
@@ -33,12 +36,12 @@ const app = firebase.initializeApp(firebaseConfig);
 
 const firestore = firebase.firestore(app); // Initialize firestore
 // Use emulator for test mode
-if (process.env.NODE_ENV === 'test') {
+if (process.env.NODE_ENV === "test") {
     firestore.useEmulator("localhost", 8080);
 }
 
 const auth = firebase.auth(app); // Initialize firebase auth
-const loggedIn$ = authState(auth).pipe(filter(user => !!user)); // Observable only return when user is logged in.
+const loggedIn$ = authState(auth).pipe(filter((user) => !!user)); // Observable only return when user is logged in.
 
 const Timestamp = firebase.firestore.Timestamp;
 
