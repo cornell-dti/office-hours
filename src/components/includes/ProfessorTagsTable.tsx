bbbc7283104a85b448fa11d5a3466aa4f604ed56
danakim21 <danakim21@gmail.com>
2021-10-25 13:09:26 -0400

    Add question even if all tags are inactive, Sort tags in alphabetical order

--- src/components/includes/ProfessorTagsTable.tsx
+++ src/components/includes/ProfessorTagsTable.tsx
@@ -3,34 +3,37 @@ import ProfessorTagsRow from './ProfessorTagsRow';
 
 import { getTagsQuery, useQuery } from '../../firehooks';
 
-
 const ProfessorTagsTable = (props: { courseId: string }) => {
     const tags = useQuery<FireTag>(props.courseId, getTagsQuery, 'tagId');
 
     return (
         <>
-            <div className="Spacing" />
-            <div className="ProfessorTagsTable">
-                <table className="Tags">
+            <div className='Spacing' />
+            <div className='ProfessorTagsTable'>
+                <table className='Tags'>
                     <tbody>
                         <tr>
                             <th>Assignment</th>
                             <th>Tags</th>
-                            <th id="statusColumn">Status</th>
+                            <th id='statusColumn'>Status</th>
                             <th>Edit</th>
                         </tr>
                     </tbody>
                     {tags
-                        .filter(tag => tag.level === 1)
-                        .map((tag, i) =>
+                        .filter((tag) => tag.level === 1)
+                        .sort((a, b) => (a.name < b.name ? 1 : -1))
+                        .map((tag, i) => (
                             <ProfessorTagsRow
                                 tag={tag}
                                 key={tag.tagId}
                                 index={i}
-                                childTags={tags.filter(t => t.parentTag && t.parentTag === tag.tagId)}
+                                childTags={tags.filter(
+                                    (t) =>
+                                        t.parentTag && t.parentTag === tag.tagId
+                                )}
                                 courseId={props.courseId}
                             />
-                        )}
+                        ))}
                 </table>
             </div>
         </>
