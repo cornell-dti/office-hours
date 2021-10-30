5c142845f45c6a48b2b3b7241089c893e58fafa8
nada-attia <nma46@cornell.edu>
2021-10-29 22:43:57 -0400

    Fix lint issues

--- src/test/local/courses.test.tsx
+++ src/test/local/courses.test.tsx
@@ -1,29 +1,26 @@
 import { expect } from 'chai';
+import sinon from 'sinon';
 import { updateCourses } from '../../firebasefunctions/courses'
 import { getDummyUser } from '../generators/dummy'
 import { firestore } from '../../firebase'
 
-const sinon = require('sinon');
 
 describe('updateCourses', () => {
-  it('is called successfully given a dummy user', () => {
+    it('is called successfully given a dummy user', () => {
 
-    const dummyUser = getDummyUser('first', 'last', [], {});
-    const stubCollection = sinon.stub(firestore, 'collection');
-    stubCollection.withArgs('users').returns({ 
-      doc: (userId:string) => {
-        update: (userUpdate: Partial<FireUser>) => true
-      } 
-    });
+        const dummyUser = getDummyUser('first', 'last', [], {});
+        const stubCollection = sinon.stub(firestore, 'collection');
+        stubCollection.withArgs('users').returns({ 
+            doc: () => {} 
+        });
 
-    const stubDoc = sinon.stub(firestore.collection('users'), 'doc');
-    stubDoc.withArgs('userId').returns({
-      update: (userUpdate: Partial<FireUser>) => true
-    });
-    sinon.stub(firestore.collection('users').doc('userId'), 'update').returns(true);
+        const stubDoc = sinon.stub(firestore.collection('users'), 'doc');
+        stubDoc.withArgs('userId').returns({
+            update: () => true
+        });
+        sinon.stub(firestore.collection('users').doc('userId'), 'update').returns(true);
     
-    expect(updateCourses('userId', dummyUser)).to.be.true;
+        expect(updateCourses('userId', dummyUser)).to.be.true;
 
-  })
+    })
 })
-
