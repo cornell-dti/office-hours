2497ffabd973b8331e6f97e919dff452dc8e589d
Alex <slipshot21@gmail.com>
2021-11-13 14:50:04 -0500

    updates to configuration

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
-import rootReducer from './reducers/index.js'
+import rootReducer from './reducers/root.js'
 
 const initialState = {};
 const middleware = [thunk];
