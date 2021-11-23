374a9ddfb3b641c6aa7d41bf277c4c30fcffd724
Nada <nma46@cornell.edu>
2021-11-23 13:14:25 -0500

    Add more unit tests  (#594)
    * Add more unit tests
    
    * Fix lint issues

---
+++ src/test/local/session.test.tsx
@@ -0,0 +1,32 @@
+// @ts-nocheck
+
+import sinon from 'sinon';
+import { deleteSession } from '../../firebasefunctions/session'
+import { firestore } from '../../firebase'
+
+
+describe('deleteSession', () => { 
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('is called successfully', () => {
+        const stubCollection = sinon.stub(firestore, 'collection');
+        stubCollection.withArgs('sessions').returns({ 
+            doc: () => {} 
+        });
+
+        const stubDoc = sinon.stub(firestore.collection('sessions'), 'doc');
+        stubDoc.withArgs('sessionId').returns({
+            delete: () => {}
+        });
+
+        const stubDelete = sinon.stub(firestore.collection('sessions').doc('sessionId'), 'delete');
+        stubDelete.returns(true);
+
+        deleteSession('sessionId')
+
+        sinon.assert.calledOnce(stubDelete);
+    })
+
+})
