bbeebeb8e61869a22f5c61a0a10ece36987dbe56
nada-attia <nma46@cornell.edu>
2021-12-13 12:10:59 -0500

    Wrote more unit tests

--- src/test/local/session.test.tsx
+++ src/test/local/session.test.tsx
@@ -1,8 +1,10 @@
 // @ts-nocheck
 
 import sinon from 'sinon';
-import { deleteSession } from '../../firebasefunctions/session'
+import { deleteSession, addSession, updateSession, getUsersFromSessions } from '../../firebasefunctions/session'
 import { firestore } from '../../firebase'
+import { getDummySession, getDummySessionSeries } from '../generators/dummysession'
+import { expect } from 'chai';
 
 
 describe('deleteSession', () => { 
@@ -27,6 +29,78 @@ describe('deleteSession', () => {
         deleteSession('sessionId')
 
         sinon.assert.calledOnce(stubDelete);
+    })
+
+})
+
+describe('addSession', () => { 
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('is called successfully', () => {
+        const dummySession = getDummySession();
+        const stubCollection = sinon.stub(firestore, 'collection');
+        stubCollection.withArgs('sessions').returns({ 
+            add: () => {} 
+        });
+
+        const stubAdd = sinon.stub(firestore.collection('sessions'), 'add');
+        stubAdd.withArgs(dummySession).returns({
+            then: () => {}
+        });
+
+        const stubThen = sinon.stub(firestore.collection('sessions').add(dummySession), 'then');
+        stubThen.returns(true);
+
+        expect(addSession(dummySession)).to.be.true;
+    })
+
+})
+
+describe('updateSession', () => { 
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('is called successfully', () => {
+        const oldSession = getDummySession();
+        const newSession = getDummySession();
+        const stubCollection = sinon.stub(firestore, 'collection');
+        stubCollection.withArgs('sessions').returns({ 
+            doc: () => {} 
+        });
+
+        const stubDoc = sinon.stub(firestore.collection('sessions'), 'doc');
+        stubDoc.withArgs(oldSession.sessionId).returns({
+            update: () => true
+        });
+        sinon.stub(firestore.collection('sessions').doc(oldSession.sessionId), 'update').returns(true);
+    
+        expect(updateSession(oldSession, newSession)).to.be.true;
+    })
+
+})
+
+describe('getUsersFromSessions', () => { 
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    it('is called successfully', () => {
+        const dummySessions = getDummySessionSeries();
+        const stubCollection = sinon.stub(firestore, 'collection');
+        stubCollection.withArgs('users').returns({ 
+            doc: () => {} 
+        });
+
+        const stubDoc = sinon.stub(firestore.collection('users'), 'doc');
+        stubDoc.withArgs('id').returns({
+            get: () => true
+        });
+        sinon.stub(firestore.collection('sessions').doc('id'), 'get').returns(true);
+    
+        expect(getUsersFromSessions(dummySessions)).to.be.true;
     })
 
 })
