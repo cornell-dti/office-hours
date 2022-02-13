e9f0ae47140f323e9d770a43f7355f7d7cf122ef
Dana Kim <dk559@cornell.edu>
2022-02-13 16:36:55 -0500

    Edit apple and google calendar export, Modal design

--- src/components/includes/CalendarExportModal.tsx
+++ src/components/includes/CalendarExportModal.tsx
@@ -2,7 +2,6 @@ import React, { Dispatch, SetStateAction } from 'react';
 import { Icon } from 'semantic-ui-react';
 import GoogleIcon from '../../media/google_icon.svg';
 import AppleIcon from '../../media/apple_icon.svg';
-// import { createNewEvent } from '../../firebasefunctions/GoogleExport';
 
 type Props = {
     showCalendarModal: boolean;
@@ -23,32 +22,7 @@ const CalendarExportModal = ({
         setShowCalendarModal(false);
     };
 
-    // const createCsv = () => {
-    //     const csvRows = [];
-    //     const headers = [
-    //         'Subject',
-    //         'Start Date',
-    //         'Start Time',
-    //         'End Date',
-    //         'End Time',
-    //         'Description',
-    //         'Location',
-    //     ];
-    //     csvRows.push(headers.join(','));
-    //     const values = [
-    //         'Final Exam',
-    //         '11/15/2021',
-    //         '10:00 AM',
-    //         '05/30/2020',
-    //         '1:00 PM',
-    //         '50 multiple choice',
-    //         '106 Valentine Place',
-    //     ];
-    //     csvRows.push(values.join(','));
-    //     return csvRows.join('\n');
-    // };
-
-    const getDateItems = (): {date: string; startTime: string; endTime: string} => {
+    const getDateItems = (): { date: string; startTime: string; endTime: string } => {
         const date = currentExportSession.startTime
             .toDate()
             .toISOString()
@@ -69,28 +43,28 @@ const CalendarExportModal = ({
             .substring(16, 22)
             .split(':')
             .join('');
-        return {date, startTime, endTime};
+        return { date, startTime, endTime };
     }
 
     const createIcs = () => {
         const icsRows = [];
-        const {date, startTime, endTime} = getDateItems();
+        const { date, startTime, endTime } = getDateItems();
 
         icsRows.push('BEGIN:VCALENDAR');
         icsRows.push('VERSION:2.0');
         icsRows.push('PRODID:-//ZContent.net//Zap Calendar 1.0//EN');
         icsRows.push('CALSCALE:GREGORIAN');
         icsRows.push('BEGIN:VEVENT');
-        icsRows.push(`SUMMARY:${course?.code} ${currentExportSession.title}`);
+        icsRows.push(`SUMMARY:${course?.code} ${currentExportSession.title ?
+            currentExportSession.title : ` Office Hour`}`);
         icsRows.push(`DTSTART:${date}T${startTime}00`);
         icsRows.push(`DTEND:${date}T${endTime}00`);
         icsRows.push(
-            `LOCATION:${
-                'building' in currentExportSession
-                    ? `${currentExportSession.building} ${currentExportSession.room}`
-                    : currentExportSession.modality === 'review'
-                        ? 'Zoom Discussion'
-                        : 'Online'
+            `LOCATION:${'building' in currentExportSession
+                ? `${currentExportSession.building} ${currentExportSession.room}`
+                : currentExportSession.modality === 'review'
+                    ? 'Zoom Discussion'
+                    : 'Online'
             }`
         );
         icsRows.push('END:VEVENT');
@@ -111,16 +85,16 @@ const CalendarExportModal = ({
     };
 
     const getGoogleCalendarLink = (): string => {
-        const title = encodeURIComponent(currentExportSession.title ? currentExportSession.title : 
-            `${course?.code} office hour`);
-        const {date, startTime, endTime} = getDateItems();
+        const title = encodeURIComponent(`${course?.code} ${currentExportSession.title ?
+            currentExportSession.title : ` Office Hour`}`);
+        const { date, startTime, endTime } = getDateItems();
         const dates = encodeURIComponent(`${date}T${startTime}00/${date}T${endTime}00`);
         const ctz = encodeURIComponent('America/New_York');
-        const location = encodeURIComponent('building' in currentExportSession ? 
+        const location = encodeURIComponent('building' in currentExportSession ?
             `${currentExportSession.building} ${currentExportSession.room}` : 'Online');
 
-        const finalLink = `https://calendar.google.com/calendar/render?action=TEMPLATE` + 
-        `&text=${title}&dates=${dates}&ctz=${ctz}&location=${location}`;
+        const finalLink = `https://calendar.google.com/calendar/render?action=TEMPLATE` +
+            `&text=${title}&dates=${dates}&ctz=${ctz}&location=${location}`;
         return finalLink;
     }
 
@@ -151,6 +125,8 @@ const CalendarExportModal = ({
                                 <a
                                     className='ExportText'
                                     href={getGoogleCalendarLink()}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
                                 >
                                     Export to Google Calendar
                                 </a>
