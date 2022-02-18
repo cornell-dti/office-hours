a6feb71bb30290ea65dfd90a714676b3bab4ccdf
Alex Nagel <slipshot21@gmail.com>
2022-02-17 23:02:29 -0500

    removing problematic .reverse()

--- src/firehooks.ts
+++ src/firehooks.ts
@@ -365,7 +365,7 @@ export const useNotificationTracker =
 
 export const useNotifications = 
 (trackerId: string | undefined): SessionNotification[] | undefined => 
-    useNotificationTracker(trackerId)?.notificationList.reverse();
+    useNotificationTracker(trackerId)?.notificationList;
 
 // Primatives
 // Look up a doc in Firebase by ID
