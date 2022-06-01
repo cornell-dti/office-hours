b97b48be17681fde1dce8c0e4387b39ae71a648b
Alex <slipshot21@gmail.com>
2022-05-31 22:23:32 -0700

    reverting semester

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const CURRENT_SEMESTER = 'SU22';
+export const CURRENT_SEMESTER = 'SP22';
 
-export const START_DATE = '2022-06-20'
-export const END_DATE = '2022-08-13'
+export const START_DATE = '2022-01-24'
+export const END_DATE = '2022-05-21'
