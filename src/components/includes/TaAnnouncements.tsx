d84fc2906e1d8dfd43cad92751b50ff0dc989c96
Dana Kim <dk559@cornell.edu>
2022-10-27 21:24:48 -0400

    Add logic for TA announcements

--- src/components/includes/TaAnnouncements.tsx
+++ src/components/includes/TaAnnouncements.tsx
@@ -1,18 +1,24 @@
 import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux';
+import { firestore } from 'firebase-admin';
 import plusCircle from '../../media/plus-circle.svg';
 import chevronUp from '../../media/chevron-up.svg'
 import chevronDown from '../../media/chevron-down.svg'
 import announcement from '../../media/announcement.svg'
+import { addTaAnnouncement, deleteTaAnnouncement } from '../../firebasefunctions/session';
+import { RootState } from '../../redux/store';
 
-const TaAnnouncements = () => {
+type Props = {
+    user: FireUser;
+    session: FireSession;
+}
 
-    // TODO: Change num, TA name 
-    const num = 0
-    const taName = "Ashley Ticzon"
+const TaAnnouncements = ({ user, session }: Props) => {
     const [showBody, setShowBody] = useState(false)
     const [showAnnouncements, setShowAnnouncements] = useState(false)
     const [showNewAnnouncement, setShowNewAnnouncement] = useState(false)
     const [announcementContent, setAnnouncementContent] = useState("")
+    const [taAnnouncements, setTaAnnouncements] = useState(session.taAnnouncemements);
 
     const clickCircleIcon = () => {
         setShowBody(true)
@@ -21,85 +27,139 @@ const TaAnnouncements = () => {
     }
 
     const collapseBody = () => {
+        if (!showBody) {
+            setShowAnnouncements(true);
+            setShowNewAnnouncement(false);
+        }
         setShowBody(state => !state)
         setShowAnnouncements(true);
     }
 
     const enterAnnouncement = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setAnnouncementContent(e.target.value)
+        setAnnouncementContent(e.target.value);
     }
 
     const onClickCancelButton = () => {
         setAnnouncementContent("")
         collapseBody();
-
     }
 
     const onClickPostButton = () => {
-        alert(announcementContent)
+        addTaAnnouncement(session, user, announcementContent);
+        setAnnouncementContent("");
+        setShowNewAnnouncement(false);
+        setShowAnnouncements(true);
+    }
+
+    const deleteAnnouncement = (announcement: string, uploadTime: FireTimestamp) => {
+        deleteTaAnnouncement(session, user, announcement, uploadTime)
+    }
+
+    const getTimeDifference = (announcement: TaAnnouncement) => {
+        const announcementTime = announcement.uploadTime.toDate().getTime();
+        const currentTime = firestore.Timestamp.now().toDate().getTime();
+        const difference = currentTime - announcementTime;
+        const minutes = Math.round(difference / 60000);
+        return [Math.floor(minutes / 60), minutes % 60];
     }
 
     useEffect(() => {
-        if (!showBody) {
-            setShowAnnouncements(true);
-            setShowNewAnnouncement(false)
-        }
-    }, [showBody])
+        setTaAnnouncements(session.taAnnouncemements);
+    }, [session])
 
     return (
         <div className="AnnouncementContainer">
             <div className="AnnouncementTop">
                 <div className="AnnouncementTitle">
-          TA announcements ({num})
+                    TA announcements ({taAnnouncements?.length})
                 </div>
                 <div className="AnnouncementIcons">
-                    <img src={plusCircle} alt="Add New Announcement" onClick={clickCircleIcon} />
+                    {((user.roles[session.courseId]) === "professor" || (user.roles[session.courseId]) === "ta")
+                        && <img src={plusCircle} alt="Add New Announcement" onClick={clickCircleIcon} />}
                     <img src={showBody ? chevronUp : chevronDown} alt="View Announcements" onClick={collapseBody} />
                 </div>
             </div>
             {showBody &&
-        <div className="AnnouncementBottom">
-            {showAnnouncements &&
-            <div>
-                {num === 0 && <span className="NoAnnouncement">
-                No announcements yet. Click on "plus" icon to make one.
-                </span>}
-            </div>
-            }
+                <div className="AnnouncementBottom">
+                    {showAnnouncements &&
+                        <div>
+                            {taAnnouncements?.length === 0 && <span className="NoAnnouncement">
+                                No announcements yet.
+                            </span>}
+                            {taAnnouncements && <span>
+                                {taAnnouncements?.map((a, i) =>
+                                    <div className="Announcement" key={i}>
+                                        <div className="AnnouncementHeading">
+                                            <div>
+                                                <img src={announcement} className="AnnouncementIcon" />
+                                                <img
+                                                    src={user ? user.photoUrl : '/placeholder.png'}
+                                                    className="AnnouncementTaPhoto"
+                                                />
+                                                {a.ta.firstName}
+                                                {a.ta.lastName}
+                                                {(a.ta.userId === user.userId) && "(You)"}
+                                            </div>
+                                            <div>{getTimeDifference(a)[0] != 0 && getTimeDifference(a)[0] + ` hour`}
+                                                {getTimeDifference(a)[1]} min ago</div>
+                                        </div>
+                                        <div className="AnnouncementContent">
+                                            {a.announcement}
+                                            {(a.ta.userId === user.userId) && <span onClick={() =>
+                                                deleteAnnouncement(a.announcement, a.uploadTime)}
+                                            >DELETE</span>}
+                                        </div>
 
-            {showNewAnnouncement &&
-            <div className="NewAnnouncement">
-                <div className="NewAnnouncementTop">
-                    <img src={announcement} alt="Announcements" />
-                    <div className="NewAnnouncementTaName">{taName} (You)</div>
-                </div>
-                <div className="NewAnnouncementMiddle">
-                    <div className="NewAnnouncementInput">
-                        <input
-                            type="text"
-                            placeholder="Enter public announcement to entire queue."
-                            value={announcementContent}
-                            onChange={enterAnnouncement}
-                            maxLength={45}
-                        />
-                    </div>
-                    <div className="NewAnnouncementLength">
-                        {announcementContent.length} / 45
-                    </div>
-                </div>
-                <div className="NewAnnouncementBottom">
-                    <span className="CancelButton" onClick={onClickCancelButton}>
-                  Cancel
-                    </span>
-                    <span className="PostButton" onClick={onClickPostButton}>
-                  Post
-                    </span>
-                </div>
-            </div>}
-        </div>}
+
+
+                                    </div>
+                                )}
+                            </span>}
+                        </div>
+                    }
+
+                    {showNewAnnouncement &&
+                        <div className="NewAnnouncement">
+                            <div className="NewAnnouncementTop">
+                                <img src={announcement} alt="Announcements" />
+                                <div className="NewAnnouncementTaName">
+                                    {`${user.firstName} ${user.lastName}`} (You)
+                                </div>
+                            </div>
+                            <div className="NewAnnouncementMiddle">
+                                <div className="NewAnnouncementInput">
+                                    <input
+                                        type="text"
+                                        placeholder="Enter public announcement to entire queue."
+                                        value={announcementContent}
+                                        onChange={enterAnnouncement}
+                                        maxLength={45}
+                                    />
+                                </div>
+                                <div className="NewAnnouncementLength">
+                                    {announcementContent.length} / 45
+                                </div>
+                            </div>
+                            <div className="NewAnnouncementBottom">
+                                <span className="CancelButton" onClick={onClickCancelButton}>
+                                    Cancel
+                                </span>
+                                <span className="PostButton" onClick={onClickPostButton}>
+                                    Post
+                                </span>
+                            </div>
+                        </div>}
+                </div>}
 
         </div>
     )
 }
 
-export default TaAnnouncements
+const mapStateToProps = (state: RootState) => ({
+    user: state.auth.user,
+    session: state.course.session,
+})
+
+export default connect(mapStateToProps, {})((props: Props) => {
+    return <TaAnnouncements {...props} />
+})
