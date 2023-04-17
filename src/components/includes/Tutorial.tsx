85672be70839cd3967ba6c793b28e10f557d7ee9
Alex Nagel <slipshot21@gmail.com>
2023-04-16 20:04:30 -0400

    basic tutorial code

---
+++ src/components/includes/Tutorial.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { RootState } from '../../redux/store';
+import {connect} from 'react-redux'
+import { CURRENT_SEMESTER, START_DATE, END_DATE } from '../../constants';
+import {importProfessorsOrTAsFromCSV} from '../../firebasefunctions/importProfessorsOrTAs';
+
+
+type Props = FireComment & {
+    user: FireUser | undefined;
+    course: FireCourse;
+}
+
+const Tutorial = ({ user, course }: Props) => {
+    // Creates a question during the tutorial to demonstrate how the screen appears when this happens
+    const createQuestion = () => {}
+    // Makes the user a TA so they can see TA view
+    const makeRole = (role: ('professor' | 'ta')) => {
+        const year = (new Date(START_DATE)).getFullYear() % 100;
+        const term = CURRENT_SEMESTER.substr(0, 2);
+        if(user !== undefined) {
+            importProfessorsOrTAsFromCSV(course, role, [user?.userId]);
+        }
+    }
+   return (<>
+   </>
+    );
+}
+
+const mapStateToProps = (state: RootState) => ({
+    user : state.auth.user,
+})
+
+
+export default connect(mapStateToProps, {})(Tutorial);
