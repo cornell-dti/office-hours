77aee72457b7bdf60b03a889fdcefd1ca98576a5
rgu0114 <richardgu0114@gmail.com>
2023-09-17 16:02:06 -0400

    update dti branding

--- src/components/includes/Footer.tsx
+++ src/components/includes/Footer.tsx
@@ -1,16 +1,16 @@
-import * as React from 'react';
-import DTILogo from '../../media/dti_logo.svg';
+import * as React from "react";
+import DTILogo from "../../media/dti_logo_glyph.svg";
 
 const Footer = () => {
     return (
         <div className="footerWrapper">
             <div className="footerTextWrapper">
                 <p className="footerText">Made by</p>
-                <img className="footerLogo" src={DTILogo} alt="DTI Logo"/>
-                <p className="footerText">{"Cornell Design \n& Tech Initiative"}</p>
+                <img className="footerLogo" src={DTILogo} alt="DTI Logo" />
+                <p className="footerText">{"Cornell Digital \n Tech & Innonvation"}</p>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Footer;
