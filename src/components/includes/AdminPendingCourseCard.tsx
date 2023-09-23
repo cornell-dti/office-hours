01698dbcd40a010c6b24b1a0f64da441ee5b8862
Sydney <sydneydtran@gmail.com>
2023-09-23 16:55:47 -0400

    Add reject and confirm pending courses

--- src/components/includes/AdminPendingCourseCard.tsx
+++ src/components/includes/AdminPendingCourseCard.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 import { Card } from '@material-ui/core';
+import { rejectPendingCourse, confirmPendingCourse } from '../../firebasefunctions/courses';
 
 import AdminReadOnlyCourseCard from './AdminReadOnlyCourseCard';
 
 const AdminPendingCourseCard = ({ course }: { readonly course: FireCourse }) => {
+    const reject = () => {
+        rejectPendingCourse(course.courseId)
+    };
+
+    const confirm = () => {
+        confirmPendingCourse(course)
+    };
+
     return (
         <Card className="course">
             <AdminReadOnlyCourseCard course={course} showSettings={false}/>
             <div>
-                {/* on click remove from pending course */}
-                <button type="button" className="pending-button reject"> 
+                <button type="button" className="pending-button reject" onClick={reject}> 
                     Reject
                 </button>
-                {/* on click create course */}
-                <button type="button" className="pending-button confirm">
+                <button type="button" className="pending-button confirm" onClick={confirm}> 
                     Confirm
                 </button>
             </div>
