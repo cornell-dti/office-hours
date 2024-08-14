cf725687b3de5ddc7e7acb713d669d92a4e800f4
rgu0114 <richardgu0114@gmail.com>
2024-08-13 21:47:35 -0700

    Update constants.ts for fa2024

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const CURRENT_SEMESTER = 'SP24';
+export const CURRENT_SEMESTER = 'FA24';
 
-export const START_DATE = '2024-01-15'
-export const END_DATE = '2024-05-20'
+export const START_DATE = '2024-08-19'
+export const END_DATE = '2024-12-20'
