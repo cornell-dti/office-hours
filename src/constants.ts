78dbf0c347484ed656adb28a8b1c5ea8fea4d4d7
Eric Weng <ew522@cornell.edu>
2024-10-27 20:38:37 -0400

    Implemented Wrapped Countdown Component and fixed code styling

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,7 @@ export const CURRENT_SEMESTER = 'FA24';
 
 export const START_DATE = '2024-08-19'
 export const END_DATE = '2024-12-20'
+
+// These are the start date and launch date for QMI Wrap for the current semester
+export const WRAPPED_START_DATE = '2024-11-05'
+export const WRAPPED_LAUNCH_DATE = '2024-11-15'
