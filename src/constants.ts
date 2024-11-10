2491f4c36e42c9bd6f4630f8b2dcd9166d739103
Eric Weng <ew522@cornell.edu>
2024-11-09 20:19:24 -0500

    Added Confetti and finalized the countdown component

--- src/constants.ts
+++ src/constants.ts
@@ -4,5 +4,5 @@ export const START_DATE = '2024-08-19'
 export const END_DATE = '2024-12-20'
 
 // These are the start date and launch date for QMI Wrap for the current semester
-export const WRAPPED_START_DATE = '2024-11-05'
-export const WRAPPED_LAUNCH_DATE = '2024-11-15'
+export const WRAPPED_START_DATE = "2024-11-12T00:00:00";
+export const WRAPPED_LAUNCH_DATE = "2024-11-22T00:00:00";
