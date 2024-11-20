c59a15407a425d27a227bfee5c699d7a5f16afa6
anchen9 <anc76@cornell.edu>
2024-11-20 16:34:06 -0500

    fix == to === on TA slide

--- src/components/includes/Wrapped.tsx
+++ src/components/includes/Wrapped.tsx
@@ -530,7 +530,7 @@ const Wrapped= (props: Props): JSX.Element => {
                     />
                     
                     <div className="timeHelped bottom-text">
-                        {wrappedData.timeHelpingStudents == 1 ? "HOUR " : "HOURS " }
+                        {wrappedData.timeHelpingStudents === 1 ? "HOUR " : "HOURS " }
                             HELPING STUDENTS
                     </div>
                 </div>
