47d4758022b01aaa477bed682cc8cbd6c49415d2
rgu0114 <richardgu0114@gmail.com>
2024-11-21 20:13:20 -0500

    merge consistency

--- src/components/includes/Wrapped.tsx
+++ src/components/includes/Wrapped.tsx
@@ -34,11 +34,6 @@ type DotProps = {
     onClick: () => void;
 };
 
-type DotIndicatorProps = {
-    showDots: string
-}
-
-  
 const Dot = ({ active, onClick } : DotProps) => (
     <div className={`dot ${active ? 'active' : ''}`} onClick={onClick}/>
 );
@@ -185,9 +180,8 @@ const Wrapped= (props: Props): JSX.Element => {
         });
     };
 
-    const DotsIndicator = ({showDots} : DotIndicatorProps) => (
-        
-        <div className={"dotsContainer" + showDots}>
+    const DotsIndicator = () => (
+        <div className="dotsContainer">
             {[...Array(totalStages)].map((_, index) => ( 
                 <Dot active={index === stage} onClick={() => setStage(index)}/>
             ))}
@@ -201,65 +195,64 @@ const Wrapped= (props: Props): JSX.Element => {
         return () => clearTimeout(timer);
     }
 
-    const WrappedAnimationModal = () =>
-        (                 
-            <div className='qmi-container'>
+    const WrappedAnimationModal = () => (                 
+        <div className='qmi-container'>
                                 
-                {/* creates first red svg circle. need linear gradient tags here 
+            {/* creates first red svg circle. need linear gradient tags here 
               since didn't import this svg.
               make note that width and height are basically the box containing the circle,
               so they need to be double the radius
               */}
-                <svg className="red-circle" width="300" height="300">
-                    {/* this creates the color gradients on the qmi logo */}
-                    <defs>
-                        <linearGradient 
-                            id="red-gradient" 
-                            x1="24.4251" 
-                            y1="-52.6352" 
-                            x2="112.279" 
-                            y2="143.659" 
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stopColor="#FF9399" />
-                            <stop offset="1" stopColor="#FF5A60" />
-                        </linearGradient>
-                    </defs>
+            <svg className="red-circle" width="300" height="300">
+                {/* this creates the color gradients on the qmi logo */}
+                <defs>
+                    <linearGradient 
+                        id="red-gradient" 
+                        x1="24.4251" 
+                        y1="-52.6352" 
+                        x2="112.279" 
+                        y2="143.659" 
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stopColor="#FF9399" />
+                        <stop offset="1" stopColor="#FF5A60" />
+                    </linearGradient>
+                </defs>
                                     
-                    {/* this is the actual circle part. 
+                {/* this is the actual circle part. 
                                     cx and cy are centers so shld be half of height/width. r is radius */}
-                    <circle cx='150' cy='150' r='115'> </circle>
-                </svg>
-                <svg className="blue-circle" width="400" height="400">
-                    <defs>
-                        <linearGradient 
-                            id="blue-gradient" 
-                            x1="36.7649" 
-                            y1="66.8832" 
-                            x2="134.326" 
-                            y2="192.278" 
-                            gradientUnits="userSpaceOnUse"
-                        >
-                            <stop stopColor="#B2D9FF" />
-                            <stop offset="1" stopColor="#78B6F4" />
-                        </linearGradient>
-                    </defs>
-                    <circle cx='200' cy='200' r='180'> </circle>
-                </svg>
-                {/* imported way of using svgs, so cant adjust stroke colors */}
-                <img src={arrow} className="arrow-circle" alt="dti arrow" />
-                {/* made two pluses since color gradient changes and second one needs
+                <circle cx='150' cy='150' r='115'> </circle>
+            </svg>
+            <svg className="blue-circle" width="400" height="400">
+                <defs>
+                    <linearGradient 
+                        id="blue-gradient" 
+                        x1="36.7649" 
+                        y1="66.8832" 
+                        x2="134.326" 
+                        y2="192.278" 
+                        gradientUnits="userSpaceOnUse"
+                    >
+                        <stop stopColor="#B2D9FF" />
+                        <stop offset="1" stopColor="#78B6F4" />
+                    </linearGradient>
+                </defs>
+                <circle cx='200' cy='200' r='180'> </circle>
+            </svg>
+            {/* imported way of using svgs, so cant adjust stroke colors */}
+            <img src={arrow} className="arrow-circle" alt="dti arrow" />
+            {/* made two pluses since color gradient changes and second one needs
                to expand outside of the container. */}
-                <img src={smallPlus} className="first-plus" alt="first plus" />
-                <img
-                    src={bigPlus}
-                    className="sec-plus"
-                    alt="second plus"
-                    onAnimationEnd={handleAnimationEnd}
-                />
+            <img src={smallPlus} className="first-plus" alt="first plus" />
+            <img
+                src={bigPlus}
+                className="sec-plus"
+                alt="second plus"
+                onAnimationEnd={handleAnimationEnd}
+            />
                 
-            </div>
-        )
+        </div>
+    )
 
     const WelcomeBanner = () => (
         <div>
