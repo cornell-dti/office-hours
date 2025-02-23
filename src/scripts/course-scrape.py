6c34e3ef2e889222a053383e993ae4cc663fd6a9
Eric Weng <ew522@cornell.edu>
2025-02-22 20:32:00 -0500

    Implemented logic for the column (Has course/profesor used QMI before) - Added blacklist courses - Added documentations to methods

--- src/scripts/course-scrape.py
+++ src/scripts/course-scrape.py
@@ -35,6 +35,59 @@ relevent_courses = [
   'STSCI'
 ]
 
+# Classes to blacklist because professors are not interested in using QMI
+blacklist = [
+   'INFO 3152',
+   'CS 3152',
+   'INFO 4400',
+   'INFO 4390'
+]
+
+def getCourseStatus(course, professors):
+    """
+    Returns the status of the course/professors in the past semester on whether they have used QMI before
+
+    2 indicates that both the course and professor have used QMI before
+    1 indicates that professor used QMI before
+    0 indicates that course used QMI before
+    -1 indicates that neither the course nor the professor have used QMI before
+
+    :param course: The course subject and its code (e.g CS 1110)
+    :param professors: The string that consist of all the professors teaching the course this sem (e.g "Michael Clarkson, Lillian Lee" )
+    """
+    with open('../scripts/trackers/past_semester.csv', 'r') as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)
+        
+        for row in csvreader:
+            if row[0] == course:
+                # List of professors in the current semester
+                current_professors = professors.split(", ")
+
+                # List of professors in the past semester
+                past_professors = row[1].split(", ")
+
+                # Checks if at least one professor in the current semester is in the past professors
+                if any(p in past_professors for p in current_professors) or professors in past_professors:
+                    # If the course previously decided to use QMI and the professor is in the past professors, return 2 (both course/prof used QMI)
+                    if row[4] == "yes":
+                        return 2
+
+                    # If the course previously decided not to use QMI and the professor is in the past professors, return the status from the past semester
+                    return row[3]
+                
+                # If the professor is not in the past professors
+                else:
+                    # If the course previously decided to use QMI, return 0 (course used QMI)
+                    if row[4] == "yes":
+                        return 0
+                    # If the course previously decided not to use QMI, but the course and professor has used QMI before, return 0 (course used QMI)
+                    elif row[4] == "no" and row[3] in ["2", "0"]:
+                        return 0
+                    return -1
+    # If no match is found, return -1 after checking all rows
+    return -1
+
 classesJSON = []
 for s in subjects:
   if s['value'] in relevent_courses:
@@ -42,22 +95,24 @@ for s in subjects:
     classesJSON.extend(grab_json(classURL)['data']['classes'])
 
 def getProfessorInfo(instructors):
+  """
+  Returns the names and emails of the professors teaching the course
+
+  :param instructors: The list of instructors teaching the course
+  """
   accum_names = "" # To accumulate all the professor names
   accum_emails = "" # To accumulate all the professor emails
   for professor in instructors:
-     # Handle middle name if exists
-    middleName = professor['middleName'] + " " if professor['middleName'] else ""
-    name = professor['firstName'] + " " + middleName + professor['lastName'] + ", "
+    name = professor['firstName'] + " " + professor['lastName'] + ", "
     email = professor['netid']  + '@cornell.edu' + ", "
     accum_names += name 
     accum_emails += email
   # Remove preceding and trailing , and "
   return accum_names.strip(', ').strip('\"'), accum_emails.strip(', ').strip('\"')
 
-
 # Some courses are not relevant 
 # We will filter out these courses if they contain these words
-irrelevant_courses = ["Independent", "Research", "Project", "Projects", "Academic Support", "Sem", "Seminar"]
+irrelevant_courses = ["Independent", "Research", "Project", "Projects", "Academic Support", "Sem", "Seminar", "Supplement", "Honors"]
 
 classes = []
 for c in classesJSON:
@@ -70,12 +125,18 @@ for c in classesJSON:
   isCredit = c['enrollGroups'][0]['unitsMinimum'] > 2
   # Course must be on campus and not Cornell Tech
   isOnCampus = c['enrollGroups'][0]['classSections'][0]['locationDescr'] == "Ithaca, NY (Main Campus)"
+
+  course = c['subject'] + " " + c['catalogNbr']
+
+  # Course must not be blacklisted
+  isNotBlacklisted = course not in blacklist
   item = {
-    'Course': c['subject'] + " " + c['catalogNbr'],
+    'Course': course,
     'Professor': names,
-    'Emails': emails
+    'Emails': emails,
+    'Has course/professor use QMI before': getCourseStatus(course, names)
   }
-  if hasProfessor and isUndergrad and isNonResearch and isCredit and isOnCampus:
+  if hasProfessor and isUndergrad and isNonResearch and isCredit and isOnCampus and isNotBlacklisted:
     classes.append(item)
 
 # Dictionary to store professor and the courses they teach
@@ -86,7 +147,8 @@ for item in classes:
     course = item['Course']
     professor = item['Professor']
     email = item['Emails']
-    key = (professor, email)
+    status = item['Has course/professor use QMI before']
+    key = (professor, email, status)
     if key in professor_courses:
         # Adds the course to the set of courses the professor teaches
         professor_courses[key].add(course)
@@ -102,24 +164,24 @@ for professor, course_list in professor_courses.items():
 
 # Output results
 if professors_with_multiple_courses:
-    print("Professors teaching multiple courses:")
-    for (professor_name, professor_email), course_list in professors_with_multiple_courses.items():
-        print(f"{professor_name} ({professor_email}) teaches: {', '.join(course_list)}")
+    for (professor_name, professor_email, status), course_list in professors_with_multiple_courses.items():
         # Remove the individual courses from the classes list that have the same professor
         for item in classes:
             if item['Course'] in course_list:
               classes.remove(item)
-        # Apoend the combined version of the courses
+        class_list = list(course_list)  # Convert set to list
+        # Append the combined version of the courses
         classes.append({
           'Course': ', '.join(course_list),
           'Professor': professor_name,
-          'Emails': professor_email
+          'Emails': professor_email,
+          'Has course/professor use QMI before': status
         })
 else:
     print("No professors teach multiple courses.")
 
-with open('../classes.csv', 'w') as file:
-    field = ["Course", "Professor", "Emails"]
+with open('../scripts/classes.csv', 'w') as file:
+    field = ["Course", "Professor", "Emails", 'Has course/professor use QMI before']
     writer = csv.DictWriter(file, fieldnames=field)
 
     # Write header
