cfd79bbaf02aa23071405716adc407b961024f92
Eric Weng <ew522@cornell.edu>
2025-04-26 18:00:38 -0400

    Fixed the feedbackprompt box size, alignment of caption, and type errors

--- src/components/types/fireData.d.ts
+++ src/components/types/fireData.d.ts
@@ -141,6 +141,7 @@ interface FireCourse {
     timeLimit?: number;
     timeWarning?: number;
     isTimeLimit?: boolean; // TODO: possibly change to non-null
+    feedbackList?: FeedbackRecord[];
 }
 
 type PrivilegedFireCourseRole = "professor" | "ta";
