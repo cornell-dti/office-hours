b5306b5ee5754492fd88b6de864037c6097d6001
NIDHI2023 <nidhisoma@gmail.com>
2025-08-10 18:10:37 -0400

    updating constants for fa25

--- src/constants.ts
+++ src/constants.ts
@@ -1,12 +1,12 @@
-export const CURRENT_SEMESTER = 'SP25';
+export const CURRENT_SEMESTER = 'FA25';
 export const ALL_SEMESTERS = ['SP20', 'FA20', 'SP21', 'FA21', 'SP22', 'FA22',
-    'SP23', 'FA23', 'SP24', 'FA24', 'SP25']
+    'SP23', 'FA23', 'SP24', 'FA24', 'SP25', 'FA25']
 
-export const START_DATE = '2025-01-15'
-export const END_DATE = '2025-05-18'
+export const START_DATE = '2025-08-19'
+export const END_DATE = '2025-12-20'
 export const MAX_EMAIL_LIMIT = 100;
 export const MAX_BATCH_LIMIT = 49;
 
 // These are the start date and launch date for QMI Wrap for the current semester
-export const WRAPPED_START_DATE = "2025-11-12T00:00:00";
-export const WRAPPED_LAUNCH_DATE = "2025-11-22T00:00:00";
+export const WRAPPED_START_DATE = "2025-11-15T00:00:00";
+export const WRAPPED_LAUNCH_DATE = "2025-11-25T00:00:00";
