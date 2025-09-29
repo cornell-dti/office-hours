5b8b2e8da12012f44c584ba60f0f1be31f585b90
Annie Chen <anc76@cornell.edu>
2025-09-29 15:52:06 -0400

    temporary mobile fix & routing fix

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -313,6 +313,13 @@ export default connect(null, { updateUser, updateAuthStatus })(({ updateUser, up
                             requireTA={true}
                         />
                         <PrivateRoute
+                            path="/ta-student-view/course/:courseId/session/:sessionId/:page?"
+                            component={TAStudentView}
+                            exact={true}
+                            requireProfessor={false}
+                            requireTA={true}
+                        />
+                        <PrivateRoute
                             path="/course/:courseId/session/:sessionId/:page?"
                             component={SplitView}
                             requireProfessor={false}
