22086df2c4e1a1c03ce477b044d05d1e32310e19
NIDHI2023 <nidhisoma@gmail.com>
2025-10-27 00:26:14 -0400

    cleans up window resizing useEffects

--- src/components/includes/TopBar.tsx
+++ src/components/includes/TopBar.tsx
@@ -107,6 +107,7 @@ const TopBar = (props: Props) => {
     // eslint-disable-next-line
     [ notificationTracker?.notificationList ]);
 
+    // Since this doesn't read in any state, we do not need to memoize it.
     const handleClick = (e: globalThis.MouseEvent) => {
         if (ref.current && !ref.current.contains(e.target as Node)) {
             setShowMenu(false);
@@ -118,7 +119,7 @@ const TopBar = (props: Props) => {
         return () => {
             document.removeEventListener("mousedown", handleClick);
         };
-    });
+    }, []);
 
     return (
         <div className="MenuBox" onBlur={() => setShowMenu(false)} >
