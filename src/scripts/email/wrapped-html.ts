8c9a1c10f55c12ad211416bd43a75f508431e8f0
NIDHI2023 <nidhisoma@gmail.com>
2025-11-18 17:13:28 -0500

    using real database link

---
+++ src/scripts/email/wrapped-html.ts
@@ -0,0 +1,62 @@
+/* eslint-disable max-len */
+export const HTML =` 
+<div style="font-family: Roboto, sans-serif; margin: 0 auto; font-size: 16px;">
+
+    <div style="text-align: center; padding-top: 40px;">
+    <img src='https://i.ibb.co/zXssGrC/wrappedemail.png' 
+    alt='Queue Me In Wrapped Header' style="height: 25%; width: 100%;"/>
+
+    </div>
+
+    <div style="padding: 20px; margin-bottom: 15px; margin-top: 15px;">
+        <p style="margin: 0; font-size: 16px;"><strong>Hey there!</strong></p>
+            <p style="line-height: 1.5; font-size: 16px;">
+            As the semester is wrapping up, our team at Queue Me In wanted to celebrate all the hard work you've put in this year — especially with office hours! <br>
+            Presenting <strong>Queue Me In Wrapped</strong>, a way for you to see your yearly office hour stats. Check it out by navigating to a class on your <a href="https://queueme.in/">home page</a> and clicking on the ribbon icon!
+            </p>
+
+            <p style="line-height: 1.5; font-size: 16px;">
+            Congratulations on all your hard work this semester!<br>
+            <i>- The Queue Me In Team</i> </p>
+        <div style="width: 100%; display: flex; justify-content: center; align-items: center; flex-wrap: wrap; gap: 10px; padding-top: 1.5em; padding-bottom: 1em; text-align: center;">
+            <img src='https://i.ibb.co/4mFzksS/Notification.png' alt="Bell Icon" style="height: 50px;"/>
+            <img src='https://i.ibb.co/4mFzksS/Notification.png' alt="Bell Icon" style="height: 50px;"/>
+            <img src='https://i.ibb.co/4mFzksS/Notification.png' alt="Bell Icon" style="height: 50px;"/>
+        </div>
+
+    </div>
+
+</div>
+
+<table width="100%" cellpadding="0" cellspacing="0" style="background-color: #025488; color: white; padding: 15px 35px; font-size: 16px; font-family: Roboto, sans-serif; display: flex; justify-content: center">
+    <tr>
+        <td align="center">
+            <!-- Container Table -->
+            <table width="600" cellpadding="0" cellspacing="0" style="width: 100%; text-align: center;">
+                <tr>
+                <!-- Heading -->
+                    <td style="text-align: center; font-family: Lato, sans-serif; padding-left: 1.5em; padding-right: 1.5em;">
+                        <p style="color: white; margin: 0; font-size: 16px; font-weight: bold;">Office Hours</p>
+                        <p style="color: white; margin: 0; font-size: 16px; font-weight: bold;">Simplified.</p>
+                    </td>
+                <!-- Logo -->
+                    <td rowspan="2" style="padding-left: 1.5em; padding-right: 2em; text-align: center; vertical-align: middle;">
+                        <img src='https://i.ibb.co/0CgQD1g/Logo-Copy-2.png' alt="QMI Logo" style="display: block; max-height: 100%; width: auto; max-width: 80px;"/>
+                    </td>
+                     <!-- Links -->
+                    <td style="padding: 10px 1.5em; text-align: left;">
+                        <p style="margin: 0; font-size: 14px; ">
+                            <a href="https://www.instagram.com/cornelldti/" style="color: white; text-decoration: underline;">DTI Instagram</a>
+                            <br>
+                            <a href="https://www.cornelldti.org/" style="color: white; text-decoration: underline;">DTI Website</a>
+                            <br>
+                            <a href="https://queueme.in/" style="color: white; text-decoration: underline;">QueueMeIn</a>
+                            <br>
+                        </p>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+`
