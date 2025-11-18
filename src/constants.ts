1b676af02211470aaeab781877399f3fe2e53521
NIDHI2023 <nidhisoma@gmail.com>
2025-11-18 17:45:59 -0500

    revert constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -10,5 +10,5 @@ export const MAX_EMAIL_LIMIT = 100;
 export const MAX_BATCH_LIMIT = 49;
 
 // These are the start date and launch date for QMI Wrap for the current semester
-export const WRAPPED_START_DATE = "2025-11-05T00:00:00";
-export const WRAPPED_LAUNCH_DATE = "2025-11-06T00:00:00";
+export const WRAPPED_START_DATE = "2025-11-23T00:00:00";
+export const WRAPPED_LAUNCH_DATE = "2025-12-03T00:00:00";
