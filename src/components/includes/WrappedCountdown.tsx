00c621a6f45512120e857b7a80f4a74374dfd459
NIDHI2023 <nidhisoma@gmail.com>
2025-11-22 11:12:12 -0500

    fixed header alignment and confetti appears each time - tested that when countdown hits 0, everything still works properly

--- src/components/includes/WrappedCountdown.tsx
+++ src/components/includes/WrappedCountdown.tsx
@@ -61,15 +61,6 @@ const WrappedCountdown: React.FC<WrappedCountdownProps> = ({ setDisplayWrapped, 
         return () => clearInterval(timer);
     }, [wrappedDate, setCountdownZero]);
 
-    // Trigger confetti with a delay only the first time `isZeroCounter` becomes true
-    useEffect(() => {
-        if (isZeroCounter && !confettiShown) {
-            setTimeout(() => {
-                setConfettiShown(true); // Show confetti after a delay
-            }, 1000); // Delay of 1 second
-        }
-    }, [isZeroCounter, confettiShown]);
-
     // Prepend the days, hours, and minutes if they're single digits
     const prependZero = (num: number): string => (num < 10 ? `0${num}` : `${num}`);
 
@@ -114,7 +105,7 @@ const WrappedCountdown: React.FC<WrappedCountdownProps> = ({ setDisplayWrapped, 
                                     View Now
                                 </div>
                             </div>
-                            {!confettiShown && <ConfettiExplosion duration={3000} force={0.6} particleCount={200} />}
+                            {isZeroCounter && <ConfettiExplosion duration={3000} force={0.6} particleCount={200} />}
                             <img className="cone" src={cone} alt="icon" />
                         </div>
                     )}
