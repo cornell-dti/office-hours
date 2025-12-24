8369183db1bb5e6f4cfb9e859c048c9f34fe40d8
NIDHI2023 <nidhisoma@gmail.com>
2025-12-23 23:34:04 -0500

    Add queue interval and character limit customization, redesigned admin pending course cards, temporarily disable email - zoho issue for emails and resend doesn't work on client side - added numeric input rule checks

--- src/components/includes/AdminPendingCourseCard.tsx
+++ src/components/includes/AdminPendingCourseCard.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Card } from "@material-ui/core";
 import emailjs from "@emailjs/browser";
+import { Resend } from 'resend';
 import { rejectPendingCourse, confirmPendingCourse } from "../../firebasefunctions/courses";
 import { addDBNotification } from "../../firebasefunctions/notifications";
 import { useUser } from "../../firehooks";
-
-import AdminReadOnlyCourseCard from "./AdminReadOnlyCourseCard";
+import 'dotenv/config'
 
 const AdminPendingCourseCard = ({ course, userId }: { readonly course: FireCourse; userId: string | undefined }) => {
     const user = useUser(userId);
@@ -17,17 +17,35 @@ const AdminPendingCourseCard = ({ course, userId }: { readonly course: FireCours
      * "rejected" or "approved")
      */
     const sendEmail = (user: FireUser, template: string) => {
-        emailjs.send(
-            "qmi",
-            "course_request_" + template,
-            {
-                course_code: course.code,
-                course_sem: course.semester,
-                to_name: user.firstName + " " + user.lastName,
-                to_email: user.email,
-            },
-            "Ye5mZ4kjPRBjUdVUB"
-        );
+        //         const resend = new Resend(process.env.REACT_APP_RESEND_API_KEY);
+        //         const subject = template === "approved" ? `Queue Me In ${course.code} ${course.semester} Approved`: `Queue Me In ${course.code} ${course.semester} Rejected`
+        //         /* eslint-disable max-len */
+        //         const message =`Hello ${user.firstName + " " + user.lastName},
+        // ${template === "approved" ? 
+        //     "Your new class request on QMI has been approved. " + course.code + " has been added to the current classes for " +course.semester + ".":
+        //     "Your new class request on QMI has been rejected. "+ course.code + " for " + course.semester + " has been removed from the pending classes. Please email QMI directly for more info."}
+
+        // Best wishes,
+        // QMI`
+
+        //   resend.emails.send({
+        //     from: 'queuemein@cornelldti.org',
+        //     to: [user.email],
+        //     subject: subject,
+        //     html: message,
+        //   });
+
+        // emailjs.send(
+        //     "qmi",
+        //     "course_request_" + template,
+        //     {
+        //         course_code: course.code,
+        //         course_sem: course.semester,
+        //         to_name: user.firstName + " " + user.lastName,
+        //         to_email: user.email,
+        //     },
+        //     "Ye5mZ4kjPRBjUdVUB"
+        // );
     };
 
     const reject = () => {
@@ -61,9 +79,52 @@ const AdminPendingCourseCard = ({ course, userId }: { readonly course: FireCours
     };
 
     return (
-        <Card className="course">
-            <AdminReadOnlyCourseCard course={course} />
-            <div>
+        <Card className="course">            
+            <div className="courseInfo">
+                <div className="courseInfoRow">
+                    <div className="field">
+                        Creator
+                    </div>
+                    <div className="value">
+                        <p>Professor {user?.firstName} {user?.lastName}</p>
+                        <div className="grayText">{user?.email}</div>
+                    </div>
+                </div>
+
+                <div className="courseInfoRow">
+                    <div className="field">
+                        Course
+                    </div>
+                    <div className="value">
+                        <p>{course.code.trim()}</p>
+                        <p>{course.name.trim()}</p>
+                    </div>
+                </div>
+
+                <div className="courseInfoRow">
+                    <div className="field">
+                        Semester
+                    </div>
+                    <div className="value">
+                        <p>{course.semester}</p>
+                    </div>
+                </div>
+
+                <div className="courseInfoRow">
+                    <div className="field">
+                        Settings
+                    </div>
+                    <div className="value">
+                        <div className="grayText">Queue Open Interval: <span>{course.queueOpenInterval}</span></div>
+                        <div className="grayText">Char Limit: <span>{course.charLimit}</span></div>
+                        <div className="grayText">Start Date: <span>{course.startDate
+                            .toDate().toLocaleDateString()}</span></div>
+                        <div className="grayText">End Date: <span>{course.endDate.toDate().
+                        toLocaleDateString()}</span></div>
+                    </div>
+                </div>
+            </div>
+            <div className="buttonRow">
                 <button type="button" className="pending-button reject" onClick={reject}>
                     Reject
                 </button>
