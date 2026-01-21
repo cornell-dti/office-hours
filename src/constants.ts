a9bf2be20f4799a246f52a6b7ddcfd0a13b68256
Chris Chen <cchen6479@gmail.com>
2026-01-21 16:59:57 -0500

    update sp26 constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,9 +1,9 @@
-export const CURRENT_SEMESTER = 'FA25';
+export const CURRENT_SEMESTER = 'SP26';
 export const ALL_SEMESTERS = ['SP20', 'FA20', 'SP21', 'FA21', 'SP22', 'FA22',
-    'SP23', 'FA23', 'SP24', 'FA24', 'SP25', 'FA25']
+    'SP23', 'FA23', 'SP24', 'FA24', 'SP25', 'FA25', 'SP26']
 
-export const START_DATE = '2025-08-19'
-export const END_DATE = '2025-12-20'
+export const START_DATE = '2026-01-20'
+export const END_DATE = '2026-05-16'
 export const MAX_EMAIL_LIMIT = 100;
 export const MAX_BATCH_LIMIT = 49;
 
