9dbaa19774a97aea1cefc147ca09d92d1367241a
Chris Chen <cchen6479@gmail.com>
2026-01-21 17:22:06 -0500

    readd constant

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,8 @@ export const CURRENT_SEMESTER = 'SP26';
 export const ALL_SEMESTERS = ['SP20', 'FA20', 'SP21', 'FA21', 'SP22', 'FA22',
     'SP23', 'FA23', 'SP24', 'FA24', 'SP25', 'FA25', 'SP26']
 
+export const MOBILE_BREAKPOINT = 920; // same as index.scss
+
 export const START_DATE = '2026-01-20'
 export const END_DATE = '2026-05-16'
 export const MAX_EMAIL_LIMIT = 100;
