e77b9a39fa50f5eaa546438dc5fbd9990367090d
Esha Shah <esha.shah2023@gmail.com>
2026-08-25 21:34:14 -0400

    update semester constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,14 +4,14 @@ export const ALL_SEMESTERS = ['SP20', 'FA20', 'SP21', 'FA21', 'SP22', 'FA22',
 
 export const MOBILE_BREAKPOINT = 920; // same as index.scss
 
-export const START_DATE = '2025-08-19'
-export const END_DATE = '2025-12-20'
+export const START_DATE = '2026-08-24'
+export const END_DATE = '2026-12-19'
 export const MAX_EMAIL_LIMIT = 100;
 export const MAX_BATCH_LIMIT = 49;
 
 // These are the start date and launch date for QMI Wrap for the current semester
-export const WRAPPED_START_DATE = "2026-11-10T00:00:00";
-export const WRAPPED_LAUNCH_DATE = "2026-11-20T00:00:00";
+export const WRAPPED_START_DATE = "2026-11-22T00:00:00";
+export const WRAPPED_LAUNCH_DATE = "2026-12-02T00:00:00";
 
 /**
  * Maximum file size allowed for file uploads (in bytes).
